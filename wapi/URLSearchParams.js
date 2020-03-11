71b75034f054ca53cdcb672c5a80c6adbb801124
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-03-11 11:14:32 +0330

    50

---
+++ wapi/URLSearchParams.js
@@ -0,0 +1,49 @@
+new URLSearchParams(init)
+.append()
+.delete()
+.entries()
+.forEach()
+.get()
+.getAll()
+.has()
+.keys()
+.set()
+.sort()
+.toString()
+.values()
+
+var p = new URLSearchParams({a:'x', b:'y'})
+p.toString() // 'a=x&b=y'
+''+p         // ...
+p+''         // ...
+
+var p = new URLSearchParams({name:'woody', topic:'api'})
+p.has('topic')                // true
+p.get('topic')                // 'api'
+p.getAll('topic')             // ['api']
+p.get('foo')                  // null
+p.append('topic', 'webdev')   // 'name=woody&topic=api&topic=webdev'
+p.set('topic', 'More webdev') // 'name=woody&topic=More+webdev'
+p.delete('topic')             // 'name=woody'
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// notes
+
+// will remove leading ? if present
+new URLSearchParams('?query=value') +'' // 'query=value'
+
+// can directly be used in `for of`
+// both equal:
+for (const [key, value] of mySearchParams) {}
+for (const [key, value] of mySearchParams.entries()) {}
+
+// does not parse full urls
+var p = new URLSearchParams('http://example.com/search?query=%40')
+p.has('query') // false 
+p.get('query') // null
+// instead:
+var url = new URL('http://example.com/search?query=%40')
+var p = new URLSearchParams(url.search)
+p.has('query') // true
+p.get('query') // '@'
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
--- wapi/fetch.js
+++ wapi/fetch.js
@@ -1,7 +1,28 @@
 fetchResponsePromise = fetch(resource, init);
 
-fetch('somefile.html').then(async res => await res.text() )
-fetch('somefile.html').then(res => res.text().then(console.log) )
+async function foo() {
+	var res = await fetch('https://jsonplaceholder.typicode.com/users');
+	return await res.text()
+}
+foo().then(console.log)
+
+fetch('some/url/path').then(async res => await res.text() ).then(console.log)
+fetch('some/url/path').then(res => res.text().then(console.log) )
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// pass query strings
+const url = new URL('some/url')
+const params = {a:'x', b:'y'}  // or  [ ['a','x'], ['b','y'] ]
+url.search = new URLSearchParams(params).toString()
+fetch(url).then()
+
+// shorthand
+fetch( 'http://foo.com?' + new URLSearchParams({a:'x', b:'y'}) )
+
+// another way
+const url = new URL('some/url')
+const params = {a:'x', b:'y'}
+Object.keys(params).forEach( key => url.searchParams.append(key, params[key]) )
+fetch(url).then()
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // json
 const response = await fetch('http://example.com/movies.json');
