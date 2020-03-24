9d25618d854a69681fb28d9ffa9a6f8fa956e68f
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-03-25 02:59:43 +0430

    59

--- canvas/evt - events (another).js
+++ canvas/evt - events (another).js
@@ -1,15 +1,15 @@
-var elem = document.getElementById('myCanvas'),
-	elemLeft = elem.offsetLeft,
-	elemTop = elem.offsetTop,
-	context = elem.getContext('2d'),
-	elements = [];
+var elem = document.getElementById('myCanvas');
+var elemLeft = elem.offsetLeft;
+var elemTop = elem.offsetTop;
+var context = elem.getContext('2d');
+var elements = [];
 
-// Add event listener for `click` events.
+// add event listener for `click` events
 elem.addEventListener('click', function (event) {
 	var x = event.pageX - elemLeft,
 		y = event.pageY - elemTop;
 
-	// Collision detection between clicked offset and element.
+	// collision detection between clicked offset and element.
 	elements.forEach(function (element) {
 		if (y > element.top &&
 			y < element.top + element.height &&
@@ -21,7 +21,7 @@ elem.addEventListener('click', function (event) {
 
 }, false);
 
-// Add element.
+// add element
 elements.push({
 	colour: '#05EFFF',
 	width: 150,
@@ -30,7 +30,7 @@ elements.push({
 	left: 15
 });
 
-// Render elements.
+// render elements
 elements.forEach(function (element) {
 	context.fillStyle = element.colour;
 	context.fillRect(element.left, element.top, element.width, element.height);
---
+++ canvas/ref.js
@@ -0,0 +1,96 @@
+HTMLCanvasElement extends HTMLElement
+HTMLCanvasElement.height
+HTMLCanvasElement.width
+HTMLCanvasElement.getContext()
+HTMLCanvasElement.toDataURL()
+HTMLCanvasElement.toBlob()
+
+CanvasRenderingContext2D
+
+// rect
+.clearRect(x, y, width, height)
+.fillRect(...)
+.strokeRect(...)
+
+// text
+.fillText(text, x, y ,?maxWidth)
+.strokeText(...)
+.measureText(text)
+
+// line style
+.lineWidth = 1.0
+.lineCap   = 'butt'|'round'|'square'
+.lineJoin  = 'bevel'|'round'|'miter'
+.miterLimit
+.lineDashOffset
+.getLineDash()
+.setLineDash(segments)
+
+// text style
+.font         = '10px sans-serif'
+.textAlign    = 'start'|'end'|'left'|'right'|'center'
+.textBaseline = 'alphabetic'|'top'|'hanging'|'middle'|'ideographic'|'bottom'
+.direction    = 'inherit'|'ltr'|'rtl'
+
+// fill & stroke style
+.fillStyle    = '#000' color|gradient|pattern
+.strokeStyle  = '#000' color|gradient|pattern
+
+// gradient & pattern
+.createLinearGradient(x0, y0, x1, y1)
+.createRadialGradient(x0, y0, r0, x1, y1, r1)
+.createPattern(image, repetition)
+
+// shadow
+.shadowBlur    = 0
+.shadowColor   = 'fully-transparent black'
+.shadowOffsetX = 0
+.shadowOffsetY = 0
+
+// path
+.beginPath()
+.closePath()
+.moveTo(x, y)
+.lineTo(x, y)
+.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y)
+.quadraticCurveTo(cpx, cpy, x, y)
+.arc(x, y, radius, startAngle, endAngle, ?anticlockwise)
+.arcTo(x1, y1, x2, y2, radius)
+.ellipse(x, y, radiusX, radiusY, rotation, startAngle, endAngle, ?anticlockwise)
+.rect(x, y, width, height)
+
+// draw path
+.fill(path|fillRule, ?fillRule)
+.stroke(?path)
+.drawFocusIfNeeded(path|element, ?element)
+.scrollPathIntoView(?path)
+.clip(path|fillRule, ?fillRule)
+.isPointInPath(path|x, x, y, ?fillRule)
+.isPointInStroke(path|x, x, y)
+
+// transform
+.getTransform
+.rotate(angle)
+.scale(x, y)
+.translate(x, y)
+.transform(a, b, c, d, e, f)
+.setTransform(matrix|a, b, c, d, e, f)
+
+// comositing
+.globalAlpha
+.globalCompositeOperation
+
+// draw image
+.drawImage(image, dx, dy)
+.drawImage(image, dx, dy, dWidth, dHeight)
+.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+
+// pixel manipulation
+.createImageData(imagedata|width, height)
+.getImageData(sx, sy, sw, sh)
+.putImageData(imageData, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight)
+
+// canvas state
+.save()
+.restore()
+.canvas
