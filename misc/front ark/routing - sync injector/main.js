d94bc8f93620ff1458f7c5bbb5546ff5bebca8a2
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-04-23 02:12:20 +0430

    87

--- misc/front ark/routing - sync injector/main.js
+++ misc/front ark/routing - sync injector/main.js
@@ -7,3 +7,13 @@ document.addEventListener('DOMContentLoaded', function () {
 	const oldBody = document.body.innerHTML;
 	document.body.innerHTML = nav + oldBody;
 });
+
+
+/* or async:
+let rdy;
+fetch('./_nav.html').then(async res => await res.text()).then(setContent);
+document.addEventListener('DOMContentLoaded', () => rdy = true);
+function setContent(html) {
+	rdy ? document.body.innerHTML = html + document.body.innerHTML : setTimeout(setContent, 50, html);
+}
+*/
