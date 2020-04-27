17413e17bc7c46e8c0317327216954abf2680623
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-04-27 19:06:18 +0430

    93

---
+++ php/http - url get variable.php
@@ -0,0 +1,7 @@
+<?php
+/*
+site.com/page.php?name=mohammad
+site.com/page.php?name=mohammad&job=developer
+index.php?a[]=item1&a[]=item2&b[]=$a
+index.php?a[42]=1&a[foo]=2&a[bar]=3
+*/
--- php/predefined constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-DIRECTORY_SEPARATOR (string)
-PATH_SEPARATOR (string) 			// Available since PHP 4.3.0. Semicolon on Windows, colon otherwise.
-SCANDIR_SORT_ASCENDING (integer) 	// Available since PHP 5.4.0.
-SCANDIR_SORT_DESCENDING (integer) 	// Available since PHP 5.4.0.
-SCANDIR_SORT_NONE (integer) 		// Available since PHP 5.4.0.
