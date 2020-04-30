40c682e1d9c0eb2aa16de514cafc8ad3059bd535
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-05-01 02:14:38 +0430

    99

---
+++ npkg/express/pkg - express-handlebars.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var xhbs  = require('express-handlebars');
+var app = express();
+
+// setup
+var config = {     // default
+	handlebars:      require('handlebars'),
+	extname:         '.handlebars',         // file extension of templates
+	layoutsDir:      'views/layouts/',      // or set via `app.locals.layout`
+	partialsDir:     'views/partials/', 
+	defaultLayout:   null,                  // by default renders without a layout
+	helpers:         null,
+	compilerOptions: null
+};
+var engine = xhbs(config); // or
+var inst = xhbs.create(config);
+
+app.engine('handlebars', engine | inst.engine);
+app.set('view engine', 'handlebars | .handlebars | .hbs' | inst.extname);
+
+app.enable('view cache'); // template caching (enabled by default if process.env.NODE_ENV = 'production')
+
+// render views
+app.get('/', function (req, res) {
+	res.render('home');
+});
+
+// pass data
+app.get('/', function (req, res) {
+  res.render('home', {
+		firstname: 'mohammad',
+		lastname: 'ahmadi',
+		email: 'ma@yahoo.com'
+		/*helpers: {
+			foo: function () { return 'foo.'; }
+		}*/
+	});
+});
+
+app.listen(3000);
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+/* layout vs view
+a layout is just a template with a {{{body}}} placeholder, sorta like a level before a view.
+so when you render a view, it goes through a layout first, then the layout passes stuff to the view
+content of the view is rendered into layout's {{{body}}}
+*/
+
+res.render('foo', { layout: 'microsite' });		// looks for a 'microsite' layout in the default layouts folder
+res.render('home', { layout: false });			// renders without layout
+
+// layou example
+/* layou example
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Example App</title>
+	</head>
+	<body>
+		{{{body}}}
+	</body>
+</html>
+*/
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
--- misc/front ark/router - fake single file server with query/main.js
+++ misc/front ark/router - fake single file server with query/main.js
@@ -1,15 +1,13 @@
 (function () {
 	const { search } = location;
-	let rdy;
 
 	if (search) {
 		const name = search.slice(1);
 		document.title = name[0].toUpperCase() + name.slice(1);
-		document.addEventListener('DOMContentLoaded', () => rdy = true);
 		fetch(`_${name}.htm`).then(async res => await res.text()).then(setContent);
 	}
 
 	function setContent(html) {
-		rdy ? document.getElementById('content').innerHTML = html : setTimeout(setContent, 50, html);
+		document.readyState !== 'loading' ? document.getElementById('content').innerHTML = html : setTimeout(setContent, 50, html);
 	}
 })();
