d8fc231bd6aa228e9c427dfa36b52bb8440240e4
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-05-05 19:37:46 +0430

    107

--- misc/front ark/recipe - single file server with template engine (apache, handlebars)/#another variation (doing a bit more)/build.js
+++ misc/front ark/recipe - single file server with template engine (apache, handlebars)/#another variation (doing a bit more)/build.js
@@ -2,13 +2,41 @@ const { writeFileSync, readFileSync, unlinkSync, readdirSync, statSync } = requi
 const { join, extname, dirname, sep } = require('path');
 const chokidar = require('chokidar');
 const Handlebars = require('handlebars');
-const uglifyjs = require('uglify-js');
+const Terser = require('terser');
 
 const srcdir = 'public/views';
 
-watch('public/views', build, {temps:false});
+watch('public/views', precompile, {temps:false});
 
-function build(settings) {
+function runJs() {
+	const depGraph = getDependencyGraph('./public/js/main.js').map(i=>i.replace('public/','')).reverse();
+	const temps = '<script src="${c.root}/lib/_templates.js"></script>\n';
+	const live = existsSync('.livereload') ? '\n'+ readFileSync('.livereload', 'utf8') : '';
+	
+	const modulepreloads = depGraph.map(i => '<link rel="modulepreload" href="${c.root}/'+i+'/>').join('\n');
+	const app = depGraph.map(i => '<script type="module" src="${c.root}/'+i+'></script>').join('\n');
+	
+	writeFileSync('./html/link-modulepreload/index.tmpl', modulepreloads);
+	writeFileSync('./html/script-app/index.tmpl', temps + app + live);
+	log('Ran js.'.green);
+}
+
+function getDependencyGraph(entry, files, result=[]) {
+	if (entry) files = [entry = join(entry)];
+	for (const file of files) {
+		const content = readFileSync(file, 'utf8');
+		const matches = content.matchAll(/import.+from\s+'(.+)'/g);
+		for (const match of matches) {
+			const groups = match.slice(1);
+			const groupsJoined = groups.map( i => join(parse(file).dir, i) );
+			result.push(...groupsJoined);
+			if (groups.length) getDependencyGraph(undefined, groupsJoined, result);
+		}
+	}
+	if (entry) return [entry, ...new Set(result)].map(i => i.replace(/\\/g, '/'));
+}
+
+function precompile(settings) {
 	const { parts, temps, localparts } = {...{parts:true, temps:true, localparts:true}, ...settings};
 	const files = getFiles(srcdir);
 	const globalPartials = files.filter(i => i.startsWith('_partials'));
@@ -17,10 +45,10 @@ function build(settings) {
 	
 	let str = '';
 	str += '(function () {\n';
-	str += 'var template = Handlebars.template;\n';
+	str += 'const template = Handlebars.template;\n';
 	
 	if (localparts) {
-		str += 'var _localPartials = Handlebars._localPartials = {};\n';
+		str += 'const _localPartials = Handlebars._localPartials = {};\n';
 		str += localPartials.reduce((a,c) => `_localPartials['${ c.split('/')[0] }'] = {};\n`, '');
 		str += localPartials.reduce((a,c) => {
 			const pageName =  c.split('/')[0];
@@ -32,7 +60,7 @@ function build(settings) {
 	}
 	
 	if (parts) {
-		str += 'var partials = Handlebars.partials;\n';
+		str += 'const partials = Handlebars.partials;\n';
 		str += globalPartials.reduce((a,c) => {
 			const name = c.replace('_partials/', '').replace(extname(c), '');
 			const content = readFileSync(join(srcdir, c), 'utf8');
@@ -42,7 +70,7 @@ function build(settings) {
 	}
 	
 	if (temps) {
-		str += 'var templates = Handlebars.templates = {};\n';
+		str += 'const templates = Handlebars.templates = {};\n';
 		str += templates.reduce((a,c) => {
 			const name = c.replace(srcdir+'/', '').replace(extname(c), '');
 			const content = readFileSync(join(srcdir, c), 'utf8');
@@ -53,9 +81,9 @@ function build(settings) {
 	str += '})();';
 	
 	
-	// const res = uglifyjs.minify(str);
-	// if (res.error) throw res.error;
-	// writeFileSync('app/precompiled.min.js', res.code);
+	/* const minified = Terser.minify(str);
+	if (minified.error) throw minified.error;
+	writeFileSync('public/app/precompiled.min.js', minified.code); */
 	writeFileSync('public/app/precompiled.min.js', str);
 	console.log('Precompiled.');
 }
