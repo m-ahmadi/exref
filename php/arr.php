4cc7592a6b468692e2cae38e0328b4af795abc39
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-05-06 22:54:05 +0430

    111

--- php/arr.php
+++ php/arr.php
@@ -6,4 +6,7 @@ array_splice($arr, array_search('cpu',$arr), 1);
 in_array(2, [1,2,3])         // true
 in_array('2', [1,2,3])       // true
 in_array('2', [1,2,3], true) // false
+
+// last item
+end([1,2,3]) // 3
 ?>
