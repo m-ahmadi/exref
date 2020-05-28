d4c4f0f0a031c77c7fa1eae6dbc3c53d8a200cf4
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-05-28 19:14:42 +0430

    136

--- angular/@Injectable.ts
+++ angular/@Injectable.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-	providedIn: 'root' | 'platform' | 'any'
+	providedIn:  // which injector (@NgModule/InjectorType) will provide it?
+		'root'     // app-level injector
+		'platform' // singleton injector shared by all applications on the page
+		'any'      // provides a unique instance in every module (including lazy modules) that injects the token
 })
 class MyService {}
 
@@ -14,3 +17,9 @@ class MyService {}
 @Component() class MyComponent {
 	constructor(public myService: MyService) {}
 }
+
+/*
+if component and service are in the same file, then order of definition matters.
+define service first, then component, otherwise run-time null reference error.
+it's possible to define component first with forwardRef()
+*/
--- angular/@NgModule.ts
+++ angular/@NgModule.ts
@@ -1,14 +1,29 @@
 import { NgModule }       from '@angular/core';
 
 @NgModule({
-	imports:         [],              // other NgModules declarables available in templates of this module
-	declarations:    [],              // declarables that belong                            to this module
-  providers:       [],              // injectable objects          available in injector  of this module
-  bootstrap:       [],              // components that are bootstrapped when this module is bootstrapped (automatically added to entryComponents)
+	imports:         [],
+		// other @NgModules to import
+		// declarables exported from these @NgModules are available in templates of this module
+		
+	declarations:    [],
+		// declarables that belong to this module
+		// a declarable must belong to only one module
+		// error if declarable exists but not declared
 	
-	entryComponents: [],              // used for adding manually-made components. (bootstrap & route components are automatically added)
-	id:              undefined | '',  // register id to use in getModuleFactory()
-	jit:             false,           // true: skip aot
+  providers:       [],
+		// injectables available in injector of this module
+		// declared dependencies to provide for a class when it's instantiated
+	
+  exports:         [],
+		// declarables of this module, available in templates of other modules (that import this module)
+	
+	bootstrap:       [],
+		// components that are bootstrapped when this module is bootstrapped (automatically added to entryComponents)
+	
+	
+	entryComponents: [],    // used for adding manually-made components. (bootstrap & route components are automatically added)
+	id:              '',    // register id to use in getModuleFactory()
+	jit:             false, // true: skip aot
 	schemas:         NO_ERRORS_SCHEMA | CUSTOM_ELEMENTS_SCHEMA,
 })
 
