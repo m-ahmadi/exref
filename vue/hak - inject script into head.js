6b9e19f91f352eff6f84dbdf1d841b1ef458c389
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-06-17 03:25:15 +0430

    162

--- vue/hak - inject script into head.js
+++ vue/hak - inject script into head.js
@@ -1,16 +1,12 @@
-// sol 1: factory component definition
+// sol 1: factory component definition (cached automatically)
 new Vue({
 	components: {
 		SomeComponent: () => new Promise((resolve, reject) => {
-			if ( [...document.head.children].filter(i => i.tagName === 'SCRIPT').find(i => /myscript/.test(i.src)) ) {
-				resolve( import('./SomeComponent.js') );
-			} else {
-				const script = document.createElement('script');
-				script.onload = () => resolve( import('./SomeComponent.js') );
-				script.async = true;
-				script.src = '/path/to/myscript.js';
-				document.head.append(script);
-			}
+			const script = document.createElement('script');
+			script.onload = () => resolve( import('./SomeComponent.js') );
+			script.async = true;
+			script.src = '/path/to/myscript.js';
+			document.head.append(script);
 		});
 	},
 })
@@ -19,20 +15,16 @@ import SomeComponent from './SomeComponent.vue';
 export default {
 	components: {
 		SomeComponent: () => new Promise((resolve, reject) => {
-			if ( [...document.head.children].filter(i => i.tagName === 'SCRIPT').find(i => /myscript/.test(i.src)) ) {
-				resolve(SomeComponent);
-			} else {
-				const script = document.createElement('script');
-				script.onload = () => resolve(SomeComponent);
-				script.async = true;
-				script.src = '/path/to/myscript.js';
-				document.head.append(script);
-			}
+			const script = document.createElement('script');
+			script.onload = () => resolve(SomeComponent);
+			script.async = true;
+			script.src = '/path/to/myscript.js';
+			document.head.append(script);
 		});
 	},
 }
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-// sol 2: async lifecycle hook (best not to use)
+// sol 2: async lifecycle hook (must be guarded manually)
 async function addHeadScript() {
 	return new Promise((resolve, reject) => {
 		if ( [...document.head.children].filter(i => i.tagName === 'SCRIPT').find(i => /myscript/.test(i.src)) ) {
