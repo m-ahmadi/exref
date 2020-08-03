6c30e9f66678061b7beb025dae5e96272367b37f
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-08-03 05:55:12 +0430

    208

--- js/arr - detect ranges in a list of numbers.js
+++ js/arr - detect ranges in a list of numbers.js
@@ -3,48 +3,36 @@ log = console.log;
 // nums = [...Array(40)].map(i=>rand()).sort((a,b)=>a-b).filter((v,i,a)=>a.indexOf(v)===i)
 nums = [12, 14, 15, 17, 18, 19, 19, 21, 28, 29, 30, 30, 31, 32, 34, 34, 36, 39, 43, 44, 48, 48, 48, 51, 52, 58, 60, 61, 67, 68, 69, 73, 73, 75, 87, 89, 94, 95, 96, 98];
 
+/* desired output: (ranges of -5 to +5)
+	[12, 14, 15, 17]
+	[18, 19, 19, 21]
+	[28, 29, 30, 30, 31, 32]
+	[34, 34, 36, 39]
+	[43, 44, 48, 48, 48]
+	[51, 52]
+	[58, 60, 61]
+	[67, 68, 69]
+	[73, 73, 75]
+	[87, 89]
+	[94, 95, 96, 98]
+*/
+
 function getRanges(nums=[], _diff=1, percent=true) {
-	const res = [];
-	const len = nums.sort((a,b) => a-b).length;
+	const ranges = [];
+	nums.sort((a,b) => a-b);
 	
-	for (let i=0; i<len; i+=1) {
+	for (let i=0; i<nums.length; i+=1) {
 		const num = nums[i];
 		const diff = percent ? perc(_diff, num) : _diff;
-		const inRanges = nums.slice(i+1).filter( j => isInRange(j, num-diff, num+diff) );
-		if (inRanges.length) {
-			res.push(inRanges);
-			const skipTo = nums.indexOf( inRanges[inRanges.length-1] );
-			if (skipTo !== -1) i = skipTo;
+		const range = nums.filter( j => isInRange(j, num-diff, num+diff) );
+		if (range.length) {
+			ranges.push(range);
+			nums = nums.slice(range.length);
+			i = -1;
 		}
 	}
-	return res;
+	return ranges;
 }
-
-/* desired output: (ranges of -5 to +5)
-[12, 14, 15, 17]
-[18, 19, 19, 21]
-[28, 29, 30, 30, 31, 32]
-[34, 34, 36, 39]
-[43, 44, 48, 48, 48]
-[51, 52, 58]
-[60, 61]
-[67, 68, 69]
-[73, 73, 75]
-[87, 89]
-[94, 95, 96, 98]
-*/
-log(nums)
-const ranges = [];
-for (let i=0; i<nums.length; i+=1) {
-	const num = nums[i];
-	const range = nums.slice(i).filter( j => isInRange(j, num-5, num+5) );
-	if (range.length) {
-		ranges.push(range);
-		i = nums.indexOf(range[range.length-1]); // i = nums.findIndex(j => j === range[range.length-1]);
-	}
-}
-log(ranges)
-
 
 function perc(percent, n) {
 	return n * (percent * 0.01);
