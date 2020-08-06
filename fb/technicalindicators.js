b9a24c3b597a233f90eacb72cda1645d8dc993ce
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-08-06 16:33:59 +0430

    216

--- fb/technicalindicators.js
+++ fb/technicalindicators.js
@@ -3,10 +3,17 @@ const ti = require('technicalindicators');
 ti.sma({period: 5, values: [1,2,3,4,5,6,7,8,9], reversedInput: true});
 ti.SMA.calculate({period: 5, values: [1,2,3,4,5,6,7,8,9]});
 
-// browser:
-// node_modules/technicalindicators/dist/browser.es6.js
-// mkdir tmp && cd tmp && npm i technicalindicators && copy /b node_modules\technicalindicators\dist\browser.es6.js ..\technicalindicators.min.js && cd ../ && rmdir tmp /s /q
+/* browser:
+cdn
+	https://cdn.jsdelivr.net/npm/technicalindicators/
+	https://cdn.jsdelivr.net/npm/technicalindicators/dist/browser.es6.js
+	https://unpkg.com/browse/technicalindicators@latest/
 
-// all fns defined on window
+local
+	node_modules/technicalindicators/dist/browser.es6.js
+	mkdir tmp && cd tmp && npm i technicalindicators && copy /b node_modules\technicalindicators\dist\browser.es6.js ..\technicalindicators.min.js && cd ../ && rmdir tmp /s /q
+*/
+
+// all fns are defined on window
 sma({period: 5, values: [1,2,3,4,5,6,7,8,9]}) // [3, 4, 5, 6, 7]
 rsa()
