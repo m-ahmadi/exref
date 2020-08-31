58dec4ca3ceb361833ccffde158122d9131eb18b
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-08-31 05:32:03 +0430

    241

--- f/svg.js
+++ f/svg.js
@@ -3,6 +3,6 @@
 
 
 SVG.on(document, 'DOMContentLoaded', function () {
-  var draw = SVG().addTo('body').size(300, 300)
-	var rect = draw.rect(100, 100).attr({ fill: '#f06' })
+  var draw = SVG().addTo('body').size(300, 300);
+	var rect = draw.rect(100, 100).attr({ fill: '#f06' });
 })
