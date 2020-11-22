e35c00af4bc3b346c7b8410d190abdf3585bc588
m-ahmadi <ahmdee18@gmail.com>
2020-11-22 10:55:25 +0330

    341

--- sql/admin - pg.sql
+++ sql/admin - pg.sql
@@ -1,9 +1,9 @@
-﻿ALTER USER postgres PASSWORD 'newpass';                     # change password of a role
-SELECT * FROM pg_stat_activity WHERE datname = 'TARGET_DB'; # see active connections to a database
-REVOKE CONNECT ON DATABASE TARGET_DB FROM public;           # prevent future connections on a database
-GRANT  CONNECT ON DATABASE TARGET_DB TO   public;           # undo abouve
+﻿ALTER USER postgres PASSWORD 'newpass';                     -- change password of a role
+SELECT * FROM pg_stat_activity WHERE datname = 'TARGET_DB'; -- see active connections to a database
+REVOKE CONNECT ON DATABASE TARGET_DB FROM public;           -- prevent future connections on a database
+GRANT  CONNECT ON DATABASE TARGET_DB TO   public;           -- undo abouve
 
-# other example about active connections
+-- other example about active connections
 SELECT pid, pg_terminate_backend(pid) 
 FROM pg_stat_activity 
 WHERE datname = current_database() AND pid <> pg_backend_pid();
