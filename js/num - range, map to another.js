be3c87146b9b31fd4b8842e07602a3ff077c6238
m-ahmadi <ahmdee18@gmail.com>
2021-05-30 06:44:41 +0430

    478

--- js/num - range, map to another.js
+++ js/num - range, map to another.js
@@ -1,7 +1,6 @@
 // map range [a,...,b] to [c,...,d]
 
-// whole range
-function scale1(nums=[], newbound=[0,1]) {
+function scale(nums=[], newbound=[0,1]) {
 	let [a,b] = [Math.min(...nums), Math.max(...nums)];
 	let [c,d] = newbound;
 	return nums.map(x => (x-a) * (d-c) / (b-a) + c);
