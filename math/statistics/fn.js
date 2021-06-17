f7c867a7de840f70ba62ec7f387947ac3f7340f6
m-ahmadi <ahmdee18@gmail.com>
2021-06-17 17:37:44 +0430

    514

--- math/statistics/fn.js
+++ math/statistics/fn.js
@@ -123,7 +123,7 @@ function stdMoment(x=[], ith=1) {
 	let [m, s, n] = [mean(x), stdv(x), x.length];
 	let sum = 0;
 	for (let i=0; i<n; i++) sum += Math.pow((x[i] - m) / s, ith);
-  return sum / n;
+	return sum / n;
 }
 
 function jarqueBera(x=[]) {
@@ -135,6 +135,71 @@ function jarqueBera(x=[]) {
 	let s = (1/n) * sum(xd.map(cube)) / pow((1/n) * sum(xdsqrd), 3/2);
 	let k = (1/n) * sum(xd.map(quad)) / sqr( (1/n) * sum(xdsqrd) );
 	return (n/6) * ( sqr(s) + ((1/4) * sqr(k-3)) );
+}
+
+function norminv(p, mean=0, stdv=1) {
+	return -1.41421356237309505 * stdv * erfcinv(2 * p) + mean;
+}
+
+function erfc(x=0) {
+	return 1 - erf(x);
+}
+
+function erfcinv(p=0) {
+	if (p >= 2) return -100;
+	if (p <= 0) return 100;
+	
+	let pp = p < 1 ? p : 2-p;
+	let t = Math.sqrt(-2 * Math.log(pp / 2));
+	let x = -0.70711 * ((2.30753+t*0.27061) / (1+t*(0.99229+t*0.04481)) - t);
+	
+	let err;
+	for (let i=0; i<2; i++) {
+		err = erfc(x) - pp;
+		x += err / (1.12837916709551257 * Math.exp(-x*x) - x*err);
+	}
+	
+	return p < 1 ? x : -x;
+}
+
+function erf(x=0) {
+	let cof = [
+		-1.3026537197817094, 6.4196979235649026e-1, 1.9476473204185836e-2,
+		-9.561514786808631e-3, -9.46595344482036e-4, 3.66839497852761e-4,
+		4.2523324806907e-5, -2.0278578112534e-5, -1.624290004647e-6,
+		1.303655835580e-6, 1.5626441722e-8, -8.5238095915e-8,
+		6.529054439e-9, 5.059343495e-9, -9.91364156e-10,
+		-2.27365122e-10, 9.6467911e-11, 2.394038e-12,
+		-6.886027e-12, 8.94487e-13, 3.13092e-13,
+		-1.12708e-13, 3.81e-16, 7.106e-15,
+		-1.523e-15, -9.4e-17, 1.21e-16,
+		-2.8e-17
+	];
+	let [isneg, d, dd] = [false, 0, 0];
+	
+	if (x < 0) {
+		x = -x;
+		isneg = true;
+	}
+	
+	let t = 2 / (2 + x);
+	let ty = 4 * t - 2;
+	let tmp;
+	
+	for(let i=cof.length-1; i>0; i--) {
+		tmp = d;
+		d = ty * d - dd + cof[i];
+		dd = tmp;
+	}
+	
+	let res = t * Math.exp(-x * x + 0.5 * (cof[0] + ty * d) - dd);
+	return isneg ? res - 1 : 1 - res;
+}
+
+function polyval(p=[], x=0) {/*Horner*/
+	let res = p[0];
+	for (let i=1, n=p.length; i<n; i++) res = res * x + p[i];
+	return res;
 }
 
 function matMul(a=[], b=[]) {
