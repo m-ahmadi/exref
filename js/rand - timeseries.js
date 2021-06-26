985f9feef1d9cfab0bf0c742c12b66ba5023ae79
m-ahmadi <ahmdee18@gmail.com>
2021-06-26 13:39:41 +0430

    522

--- js/rand - timeseries.js
+++ js/rand - timeseries.js
@@ -1,17 +1,16 @@
 function randomBars(len=400, seed=100) {
-	const bars = [seed];
-	const randInt = (n,x) => (n=Math.ceil(n), x=Math.floor(x), Math.floor(Math.random()*(x-n))+n);
-	for (let i=1; i<len; i+=1) {
-		const prev = bars[i-1];
-		const r = prev * 0.05;
-		let value = randInt(prev-r, prev+r);
-		
+	let bars = [seed];
+	let randInt = (n,x) => (n=Math.ceil(n), x=Math.floor(x), Math.floor(Math.random()*(x-n))+n);
+	for (let i=1; i<len; i++) {
+		let prev = bars[i-1];
+		let r = prev * 0.05;
+		let val = randInt(prev-r, prev+r);
 		let k = 2;
-		while (value === prev) {
-			value = randInt(prev-r, prev+r*k);
+		while (val === prev) {
+			val = randInt(prev-r, prev+r*k);
 			k *=2;
 		}
-		bars.push(value);
+		bars.push(val);
 	}
 	return bars;
 }
