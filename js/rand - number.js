3ed80990aec1b03c818cf35e1a41ca7d7fe5f0b0
m-ahmadi <ahmdee18@gmail.com>
2021-06-29 20:21:27 +0430

    525

--- js/rand - number.js
+++ js/rand - number.js
@@ -1,19 +1,15 @@
-// float between 0 and 1
-Math.random();  
+Math.random(); // float between 0 (inclusive) and 1 (exclusive)
 
-// float between min and max
-function randFloat(min=0, max=10) {
-	return Math.random() * (max - min) + min;
+function randFloat(min=0, max=1) {
+	return Math.random() * (max - min) + min; // min: inclusive,  max: exclusive
 }
 
-// int between min and max
-function randInt(min=0, max=10, inclusive=true) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor( Math.random() * (max - min) ) + min;
-	return Math.floor( Math.random() * (max - min + 1) ) + min; // inclusive (including max)
+function randInt(min=0, max=1, inclusiveMax=false) {
+	[min, max] = [Math.ceil(min), Math.floor(max)];
+	return Math.floor( Math.random() * (max - min) ) + min;     // min: inclusive,  max: exclusive
+	return Math.floor( Math.random() * (max - min + 1) ) + min; // min: inclusive,  max: inclusive
 	
-	return Math.floor( Math.random() * (max - min + (inclusive?1:0)) ) + min;
+	return Math.floor( Math.random() * (max - min + (inclusiveMax?1:0)) ) + min;
 }
 
 // using Math.round() for rounding instead of ceil and floor, will give you a non-uniform distribution
