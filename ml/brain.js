29cccc404c64629a94096de92c9fdd7ce08252b3
m-ahmadi <ahmdee18@gmail.com>
2021-07-28 03:03:51 +0430

    575

--- ml/brain.js
+++ ml/brain.js
@@ -72,48 +72,45 @@ RNNTimeStep | LSTMTimeStep | GRUTimeStep
 // examples
 
 // xor
-const data = [
+var net = new brain.NeuralNetwork();
+net.train([
 	{ input: [0,0], output: [0] },
 	{ input: [0,1], output: [1] },
 	{ input: [1,0], output: [1] },
 	{ input: [1,1], output: [0] }
-];
-const net = new brain.NeuralNetwork();
-net.train(data);
-const output = net.run([1, 0]);  // [0.987]
+]);
+net.run([1,0]); // [0.987]
 
 // nn
-const net = new brain.NeuralNetwork();
-const data = [
+var net = new brain.NeuralNetwork();
+net.train([
 	{ input: [0,0,0], output: [0] },
 	{ input: [0,0,1], output: [0] },
 	{ input: [0,1,1], output: [0] },
 	{ input: [1,0,1], output: [1] },
 	{ input: [1,1,1], output: [1] }
-];
-net.train(data);
+]);
 net.run([1,0,0]) // 0.90 (probability of a new item based on trained data)
 net.run([0,0,0]) // 0.05
 net.run([0,1,1]) // 0.06
 net.run([1,1,1]) // 0.91
 
 // rnn
-const net = new brain.recurrent.RNN();
-const data = [
+var net = new brain.recurrent.RNN();
+net.train([
 	{ input: [0,0], output: [0] },
 	{ input: [0,1], output: [1] },
 	{ input: [1,0], output: [1] },
 	{ input: [1,1], output: [0] }
-];
-net.train(data);
+]);
 net.run([0,0]) // [0]
 net.run([0,1]) // [1]
 net.run([1,0]) // [1]
 net.run([1,1]) // [0]
 
 // nn - 4 teams play football (output: index of the winner team)
-const net = new brain.NeuralNetwork();
-const data = [
+var net = new brain.NeuralNetwork();
+net.train([
 	{ input: [1,2], output: [1] }, // team 2 wins
 	{ input: [1,3], output: [1] }, // team 3 wins
 	{ input: [2,3], output: [0] }, // team 2 wins
@@ -121,7 +118,7 @@ const data = [
 	{ input: [1,2], output: [0] }, // team 1 wins
 	{ input: [1,3], output: [0] }, // team 1 wins
 	{ input: [3,4], output: [0] }  // team 3 wins
-];
+]);
 /* stats: G=games, W=win, L=loss, D=draw, S=score
 .        G W L D  S
 team 1:  4 2 2 0  6
@@ -129,7 +126,6 @@ team 2:  4 2 2 0  6
 team 3:  4 2 2 0  6
 team 4:  2 1 1 0  3
 */
-net.train(data);
 // probability of who wins the next match between 2 teams:
 net.run([1,4]) // 0.49 (50% chance of winning for both teams)
 net.run([2,4]) // 0.96 (96% chance of winnning for team 4)
@@ -137,20 +133,44 @@ net.run([3,4]) // 0.01
 net.run([1,2]) // 0.48
 
 // color
-const net = new brain.NeuralNetwork();
-const data = [
+var net = new brain.NeuralNetwork();
+net.train([
 	{ input: {r:0.03, g:0.7,  b:0.5}, output: {black: 1} },
 	{ input: {r:0.16, g:0.09, b:0.2}, output: {white: 1} },
 	{ input: {r:0.5,  g:0.5,  b:1.0}, output: {white: 1} }
-];
-net.train(data1);
-net.run({ r: 1, g: 0.4, b: 0 });  // { white: 0.99, black: 0.002 }
+]);
+net.run({r:1, g:0.4, b:0});  // { white: 0.99, black: 0.002 }
 
-const net = new brain.NeuralNetwork();
-const data = [
+var net = new brain.NeuralNetwork();
+net.train([
 	{ input: {r:0.03, g:0.7},         output: {black: 1} },
 	{ input: {r:0.16, b:0.2},         output: {white: 1} },
 	{ input: {r:0.5,  g:0.5, b: 1.0}, output: {white: 1} }
-];
-net.train(data2);
-net.run({ r: 1, g: 0.4, b: 0 });  // { white: 0.81, black: 0.18 }
+]);
+net.run({ r:1, g:0.4, b:0 });  // { white: 0.81, black: 0.18 }
+
+// one-hot vector
+var net = new brain.NeuralNetwork();
+net.train([
+  {input: [1,1], output: [0,0,1]},
+  {input: [1,1], output: [0,0,1]},
+  {input: [1,1], output: [0,0,1]},
+  {input: [1,1], output: [0,0,1]},
+  {input: [1,1], output: [0,0,1]},
+  
+  {input: [0,0], output: [1,0,0]},
+  {input: [0,0], output: [1,0,0]},
+  {input: [0,0], output: [1,0,0]},
+  {input: [0,0], output: [1,0,0]},
+  {input: [0,0], output: [1,0,0]},
+	
+  {input: [1,0], output: [0,1,0]},
+  {input: [1,0], output: [0,1,0]},
+  {input: [0,1], output: [0,1,0]},
+  {input: [0,1], output: [0,1,0]},
+  {input: [0,1], output: [0,1,0]},
+]);
+net.run([1,1]) // [0.01,  0.10,  0.95]
+net.run([0,0]) // [0.95,  0.09,  0.01]
+net.run([1,0]) // [0.07,  0.87,  0.05]
+net.run([0,1]) // [0.06,  0.89,  0.05]
