7bc51cb0ae96ac14f069d25dffaa9a3310315b36
m-ahmadi <ahmdee18@gmail.com>
2021-10-29 02:10:01 +0330

    668

--- ml/tf/sol - model.predict() loop issue.py
+++ ml/tf/sol - model.predict() loop issue.py
@@ -1,11 +1,59 @@
+# caused by changing shape of input (probably, just predict at once instead)
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
+import numpy as np
+from numpy.random import uniform, randint
 from timeit import default_timer as timer
 
-# tf.compat.v1.disable_eager_execution()
+tf.compat.v1.disable_eager_execution() # no "5 out of the last 5 calls"
 
-# why: changing shape of input (probably)
+train = 1
+mods = []
+labels = [ [randint(2) for _ in range(5)] for _ in range(10) ]
+
+for i in range(1,11):
+	
+	if train == 1:
+		x = []
+		y = []
+		for j in range(10):
+			x.append( list( uniform(0,1,i) ) )
+			y.append( labels[ randint(len(labels)) ] )
+		
+		if i == 1:
+			x = [i[0] for i in x]
+		
+		model = tf.keras.Sequential([
+			tf.keras.layers.Dense(units=3, activation='sigmoid', input_shape=(i,)),
+			tf.keras.layers.Dense(units=5, activation='sigmoid')
+		])
+		model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
+		t1 = timer()
+		model.fit(np.array(x), np.array(y), batch_size=1, epochs=30, verbose=0)
+		print('mod'+str(i) + ' took', round((timer()-t1)/60, 2), 'min')
+		model.save('model'+str(i))
+
+	else:
+		model = tf.keras.models.load_model('model'+str(i))
+	
+	mods.append(model)
+
+for i in range(9):
+	input = np.array([ uniform(0,1,i+1) ])
+	print(
+		'\ninput: ', input,
+		'\npredict:', mods[i].predict(input)
+	)
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# another example (simple)
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
+import numpy as np
+from timeit import default_timer as timer
+
+tf.compat.v1.disable_eager_execution() # no "5 out of the last 5 calls"
 
 data = [
 	[ [0,0], [0] ],
@@ -26,12 +74,12 @@ for i in range(10):
 	model.compile(optimizer=tf.keras.optimizers.SGD(0.6), loss='mean_squared_error')
 	
 	t1 = timer()
-	model.fit(x, y, batch_size=1, epochs=20, verbose=0)
+	model.fit(np.array(x), np.array(y), batch_size=1, epochs=20, verbose=0)
 	print('net'+str(i) + ' done in', round((timer()-t1)/60, 2), 'min')
 	
 	mods.append(model)
 
 for i in range(10):
 	print(
-		mods[i].predict(  [ [0,0], [0,1], [1,0], [1,1] ]  )
+		mods[i].predict(  np.array([ [0,0], [0,1], [1,0], [1,1] ])  )
 	)
--- py/list.py
+++ py/list.py
@@ -1,24 +1,27 @@
 a = [1,2]
 a.append(3)
-a[4]                 # err
-len(a)               # 2
-[1,2] == [1,2]       # val equal: True
-[1,2] * 2            # list repeat: [1,2,1,2]
-[*a]                 # list unpack: [1,2]
-[*range(4)]          # ...: [1,2,3,4]
-[1,2] + [3,4]        # list concat: [1,2,3,4]
-[ *[1,2], *[3,4] ]   # ...
-[1,2].append(3)      # NoneType,  a: [1,2,3]
-[1,2,3].insert(2, 4) # NoneType,  a: [1,2,4,3]
-[1,2].extend([3,4])  # NoneType,  a: [1,2,3,4]
-[7,8,9].remove(8)    # NoneType,  a: [7,8]
-[1,2,3].reverse()    # NoneType,  a: [3,2,1]
-[3,1,2].sort()       # NoneType,  a: [1,2,3]
-[1,2,3,3].count(3)   # 2
-[1,2].index(2)       # 1
-[1,2].index(3)       # err
-[1,1,2].index(1,1)   # 1
-['a','b'].index('a') # 0
+a[4]                    # err
+len(a)                  # 2
+[1,2] == [1,2]          # val equal: True
+[1,2] * 2               # list repeat: [1,2,1,2]
+[*a]                    # list unpack: [1,2]
+[*range(4)]             # ...: [1,2,3,4]
+[1,2] + [3,4]           # list concat: [1,2,3,4]
+[ *[1,2], *[3,4] ]      # ...
+[1,2].append(3)         # NoneType,  a: [1,2,3]
+[1,2,3].insert(2, 4)    # NoneType,  a: [1,2,4,3]
+[1,2].extend([3,4])     # NoneType,  a: [1,2,3,4]
+[7,8,9].remove(8)       # NoneType,  a: [7,8]
+[1,2,3].reverse()       # NoneType,  a: [3,2,1]
+list(reversed([1,2,3])) # [3,2,1]
+[*reversed([1,2,3])]    # ...
+[1,2,3][::-1]           # ...
+[3,1,2].sort()          # NoneType,  a: [1,2,3]
+[1,2,3,3].count(3)      # 2
+[1,2].index(2)          # 1
+[1,2].index(3)          # err
+[1,1,2].index(1,1)      # 1
+['a','b'].index('a')    # 0
 a = [3]
 a.append(4) # [3,4]
 a.pop()     # 4,  a: [3]
@@ -42,7 +45,8 @@ a[:2]    # [1,2]
 a[2:]    # [3,4]
 a[-3:-1] # [2,3]
 a[:]     # copy of arr
-[1,2,3,4,5,6][::2] # [1,3,5]
+[1,2,3,4,5,6][::2]  # [1,3,5]
+[1,2,3,4,5,6][::-1] # [6,5,4,3,2,1]
 
 # list comprehension
 nums = [1,2,3,4]
