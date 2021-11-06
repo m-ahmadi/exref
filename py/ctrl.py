1884d10952878329363bbc1d75de30b671299d3e
m-ahmadi <ahmdee18@gmail.com>
2021-11-07 01:17:49 +0330

    692

--- py/ctrl.py
+++ py/ctrl.py
@@ -2,3 +2,11 @@
 # `with` ensures resource is 'cleaned up' afterward (wrap execution of block with methods defined by a context manager)
 with open('file.txt') as f:
 	f.read()
+
+# no variable scope in conditionals or loops (vars are scoped to innermost function/class/module)
+if condition:
+	x = 1 # global variable
+for i in range(2):
+	x = 2 # ...
+while True:
+	x = 2 # ...
--- pypi/pandas.py
+++ pypi/pandas.py
@@ -2,9 +2,12 @@ import pandas as pd # pip install pandas
 
 pd.read_csv('file.csv', header=None, nrows=5)
 
-DataFrame.filter(items=None, like=None|'', regex=None|'', axis=None|0|'')
+DataFrame.filter(items=None, like=None|'', regex=None|'', axis=None|0|''|'columns')
 
 DataFrame.sort_values(by=''|['',..], axis=0, ascending=True|[True,..], inplace=False, kind='quicksort|mergesort|heapsort|stable', na_position='last|first', ignore_index=False, ?key=None)
+
+L = '' | 0 | [''|0,..]
+DataFrame.drop(labels=None|L, axis=0, index=None|L, columns=None|L, level=None, inplace=False, errors='raise|ignore')
 
 DataFrameGroupBy.filter(func, dropna=True, *args, **kwargs)
 
@@ -21,3 +24,8 @@ df = pd.DataFrame([[1,2,3], [4,5,6]], index=['mouse','rabbit'], columns=['one','
 df.filter(items=['one', 'three'])
 df.filter(regex='e$', axis=1)
 df.filter(like='bbi', axis=0)
+
+# drop
+df = pd.DateFrame([ [1,2], [3,4], [5,6], [7,8] ])
+df = df.drop([2,3])
+df # [ [1,2], [3,4] ]
