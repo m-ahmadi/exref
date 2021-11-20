69aac7aa9e690772651c36c142fafb7131695f7a
m-ahmadi <ahmdee18@gmail.com>
2021-11-20 15:16:55 +0330

    718

--- ml/sklearn.py
+++ ml/sklearn.py
@@ -27,3 +27,24 @@ minmax_scale([0,10,4,5], (0,1)) # [0, 1, .4, .5]
 # single number
 y = minmax_scale([x,a,b], (c,d))[0] # transform x in range [a,b] to y in [c,d]
 minmax_scale([5,1,10], (10,100))[0] # 50
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# model_selection
+# https://scikit-learn.org/stable/modules/classes.html#module-sklearn.model_selection
+
+# K-Folds cross-validator
+sklearn.model_selection.KFold(n_splits=5, *, shuffle=False, random_state=None|0)
+
+import numpy as np
+from sklearn.model_selection import KFold
+
+data = np.array([1,2,3,4,5,6,7,8,9])
+kfold = KFold(n_splits=3)
+
+for train, test in kfold.split(data):
+	print('train:', data[train], 'test:', data[test])
+'''
+train: [4 5 6 7 8 9] test: [1 2 3]
+train: [1 2 3 7 8 9] test: [4 5 6]
+train: [1 2 3 4 5 6] test: [7 8 9]
+'''
