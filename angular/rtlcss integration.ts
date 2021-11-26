ac6b7aa0eb88e0d0697a1b596c171edb9ee8b97d
m-ahmadi <ahmdee18@gmail.com>
2021-11-26 16:31:46 +0330

    728

--- angular/rtlcss integration.ts
+++ angular/rtlcss integration.ts
@@ -1,6 +1,6 @@
 /*
 	ng add ngx-build-plus
-	npm i rtlcss autoprefixer -D
+	npm i postcss postcss-loader rtlcss autoprefixer -D
 	ng serve --extra-webpack-config webpack.partial.js -o
 */
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -14,7 +14,12 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          { loader: 'postcss-loader', options: {plugins: [autoprefixer, rtlcss]} },
+          {
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: { plugins: [autoprefixer, rtlcss] }
+						}
+					},
           'sass-loader', // ?
         ],
       },
