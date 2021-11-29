474278d764d043f9ef34ac7e9462aba48ed5a87c
m-ahmadi <ahmdee18@gmail.com>
2021-11-30 01:26:36 +0330

    733

--- py/num.py
+++ py/num.py
@@ -3,6 +3,16 @@
 4 ** 2  # squared: 16
 4 ** 3  # cubed: 64
 
+# check for whole number
+2.1 % 1 == 0 # False
+2.0 % 1 == 0 # Tru
+
+(2.2).is_integer() # False
+(2.0).is_integer() # True
+
+2.3 == int(2.3) # False
+2.0 == int(2.0) # True
+
 import math
 float('inf')  == math.inf  # True
 float('-inf') == -math.inf # True
