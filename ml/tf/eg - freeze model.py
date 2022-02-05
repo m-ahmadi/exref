ac8b4f9bad8656617c82e604b042dc4903a29ed0
m-ahmadi <ahmdee18@gmail.com>
2022-02-05 20:31:58 +0330

    783

--- ml/tf/eg - freeze model.py
+++ ml/tf/eg - freeze model.py
@@ -22,25 +22,44 @@ print(
 
 # make frozen model
 
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# convert
+
+# convert keras model to ConcreteFunction
 full_model = tf.function(lambda x: model(x))
 full_model = full_model.get_concrete_function(x=tf.TensorSpec(model.inputs[0].shape, model.inputs[0].dtype))
+
+# get frozen ConcreteFunction
 frozen_func = convert_variables_to_constants_v2(full_model)
 frozen_func.graph.as_graph_def()
-tf.io.write_graph(graph_or_graph_def=frozen_func.graph, logdir='./model_frozen', name='saved_model_frozen.pb', as_text=False)
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# save
 
+# save frozen graph from frozen ConcreteFunction
+tf.io.write_graph(graph_or_graph_def=frozen_func.graph, logdir='./model_frozen', name='saved_model_frozen.pb', as_text=False)
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# load
+
+# load frozen graph using tf v1 functions
 with tf.io.gfile.GFile('./model_frozen/saved_model_frozen.pb', 'rb') as f:
 	graph_def = tf.compat.v1.GraphDef()
 	loaded = graph_def.ParseFromString(f.read())
 
+# wrap frozen graph to ConcreteFunctions
 wrapped_import = tf.compat.v1.wrap_function(lambda: tf.compat.v1.import_graph_def(graph_def, name=''), [])
+
 import_graph = wrapped_import.graph
+
 frozen_func = wrapped_import.prune(
 	tf.nest.map_structure(import_graph.as_graph_element, ['x:0']),
 	tf.nest.map_structure(import_graph.as_graph_element, ['Identity:0'])
 )
 # layers = [op.name for op in frozen_func.graph.get_operations()]
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# predict
 
 print(
 	'\nfrozen model:\n\n',
 	frozen_func(x=tf.constant([ [0,0], [0,1], [1,0], [1,1] ], dtype='float32'))[0].numpy()
 )
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
