1666e3e25feda665b2a878652e766f56546620ca
m-ahmadi <ahmdee18@gmail.com>
2022-05-02 22:22:42 +0430

    860

---
+++ ta/ind - mv.js
@@ -0,0 +1,20 @@
+// https://www.marketvolume.com/technicalanalysis
+
+r = {};
+group = '';
+
+[...document.querySelectorAll('table.table.table-striped.its-c > tbody:nth-child(2) > tr')].forEach(i => {
+  let [, child2] = i.children;
+  let h4 = child2.querySelector('h4');
+  let p = child2.querySelector('p');
+  
+  if (h4) group = h4.innerText;
+  
+  if (p) {
+    if (!r[group]) r[group] = [];
+    r[group].push(p.querySelector('a').title);
+  }
+  
+});
+
+console.log(  Object.keys(r).map(k=> [k, ...r[k].map(j=>'\t'+j)].join('\n')).join('\n\n')  );
