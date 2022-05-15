3f4ef8f267c55441f4855a3da316565db68b2448
m-ahmadi <ahmdee18@gmail.com>
2022-05-15 06:14:46 +0430

    885

---
+++ js/arr - count item occurrences.js
@@ -0,0 +1,9 @@
+count = {};
+[1,2,2,3,4,5,5,5].map((v,i) => count[v] = (count[v] || 0) + 1);
+
+
+count = [1,2,2,3,4,5,5,5].reduce((r,v,i) => (r[v] = (r[v] || 0) + 1, r), {});
+
+
+count = [1,2,2,3,4,5,5,5].reduce((r,v) => (r.set(v, (r.get(v) || 0) + 1), r), new Map());
+[...count].map(i=>i[1]);
--- ml/fn.js
+++ ml/fn.js
@@ -1,1 +1,16 @@
 sigmoid = x => 1 / (1 + Math.pow(Math.E, -x));
+
+
+function computeClassWeight(y=[]) {
+	// y = [...Array(2000)].map((_,i)=> i<500?1:0);
+	let count = y.reduce((r,v) => (r.set(v, (r.get(v) || 0) + 1), r), new Map());
+	count = [...count].map(i=> i[1]);
+	
+	let nClasses = count.length;
+	let nSamples = y.length;
+	
+	let a = count.map(i=> nSamples / (nClasses * i));
+	let o = Object.fromEntries(a.entries());
+	
+	return o;
+}
