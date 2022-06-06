27f7c476bdcaf22aae6cec3adee44447668eb0df
m-ahmadi <ahmdee18@gmail.com>
2022-06-06 21:33:31 +0430

    918

--- ml/sktime.py
+++ ml/sktime.py
@@ -6,9 +6,14 @@ TimeSeriesForestClassifier(n_estimators=200, min_intervalint=3, n_jobs=1, random
 	.predict(X, **kwargs)
 	.predict_proba(X, **kwargs)
 
-
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 X_train, y_train = load_unit_test(split='train', return_X_y=True)
 X_test, y_test = load_unit_test(split='test', return_X_y=True)
+
+''' input shape
+x = [ [list(j) for j in i] for i in X_train.to_numpy()]
+y = list(y_train)
+'''
 
 clf = TimeSeriesForestClassifier(n_estimators=5)
 clf.fit(X_train, y_train)
