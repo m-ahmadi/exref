06f83493a023aee7511dd48a459bdc9ebddbcdaa
m-ahmadi <ahmdee18@gmail.com>
2022-06-13 17:16:32 +0430

    934

---
+++ math/statistics/test - dickey-fuller.py
@@ -0,0 +1,6 @@
+from statsmodels.tsa.stattools import adfuller
+# H₀ = unit root exists
+# H₁ = unit root does not exist
+adf, pval, _ = adfuller(x)
+adfuller([1,2,1,2,1,2,1])        # -2848326498395271.5,  0.0
+adfuller([1,50,10,70,60,90,150]) # 1.5556828641718772,   0.9977183379488197
