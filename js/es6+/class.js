9aa2cd2de2ba18d258a3e28fb06b731394967709
m-ahmadi <ahmdee18@gmail.com>
2022-06-22 09:15:10 +0430

    954

--- js/es6+/class.js
+++ js/es6+/class.js
@@ -1,52 +1,57 @@
 class Rectangle {
 	// field declarations:
 	width = 0; // public
-  height;
-	#left = 0;  // private
-  #right;
-  constructor(width, height) {
+	height;
+	#left = 0; // private
+	#right;
+	constructor(width, height) {
 		// instance properties: (must be defined inside of class methods)
 		this.width = width;
-    this.height = height;
-  }
-  // method:
-  calcArea() {
-    return this.width * this.height;
-  }
+		this.height = height;
+	}
+	// method:
+	calcArea() {
+		let foo = doSomething();
+		return this.width * this.height;
+	}
+	// private method
+	#doSomething() {
+		return this.#right - this.#left;
+	}
 	// static method: (called without instantiating the class and cannot be called through a class instance.)
 	static distance(a, b) {
 		const dx = a.x - b.x;
-    const dy = a.y - b.y;
-    return Math.hypot(dx, dy);
-  }
+		const dy = a.y - b.y;
+		return Math.hypot(dx, dy);
+	}
 	// getter:
-  get area() {
-    return this.calcArea();
-  }
+	get area() {
+		return this.calcArea();
+	}
 }
 const square = new Rectangle(10, 10);
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // subclass
 class Animal { 
-  constructor(name) {
-    this.name = name;
-  }
-  speak() {
-    console.log(`${this.name} makes a noise.`);
-  }
+	constructor(name) {
+		this.name = name;
+	}
+	speak() {
+		console.log(`${this.name} makes a noise.`);
+	}
 }
 
 class Dog extends Animal {
-  constructor(name) {
-    super(name); // all subclasses must call super() in their constructor
-  }
-  speak() {
-    console.log(`${this.name} barks.`);
-  }
+	constructor(name) {
+		super(name); // all subclasses must call super() in their constructor
+	}
 	speak() {
-    super.speak(); // super call
-    console.log(`${this.name} smiles.`);
-  }
+		console.log(`${this.name} barks.`);
+	}
+	speak() {
+		super.speak(); // super call
+		console.log(`${this.name} smiles.`);
+	}
 }
 let d = new Dog('Mitzie');
 d.speak();
@@ -57,17 +62,17 @@ d.speak();
 
 // unnamed:
 let Rectangle = class {
-  constructor(height, width) {
-    this.height = height;
-    this.width = width;
-  }
+	constructor(height, width) {
+		this.height = height;
+		this.width = width;
+	}
 };
 // named:
 let Rectangle = class Rectangle2 {
-  constructor(height, width) {
-    this.height = height;
-    this.width = width;
-  }
+	constructor(height, width) {
+		this.height = height;
+		this.width = width;
+	}
 };
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // not hoisted
@@ -77,11 +82,11 @@ class Rectangle {}
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // mixin
 const calculatorMixin = Base => class extends Base {
-  calc() {}
+	calc() {}
 };
 
 const randomizerMixin = Base => class extends Base {
-  randomize() {}
+	randomize() {}
 };
 
 class Foo {}
