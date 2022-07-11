0282c35ab3a0b6e08ff57980b1473e3d952e9d41
m-ahmadi <ahmdee18@gmail.com>
2022-07-11 22:25:29 +0430

    972

--- vscode/run-after-update.cmd
+++ vscode/run-after-update.cmd
@@ -1,6 +1,8 @@
 @echo off
 
-for %%i in (HKCR\*\shell\VSCode, HKCR\Directory\shell\VSCode, HKCR\Directory\Background\shell\VSCode, HKCR\Drive\shell\VSCode) do reg delete %%i /f
+reg delete HKCR\*\shell\VSCode /f
+
+for %%i in (HKCR\Directory\shell\VSCode, HKCR\Directory\Background\shell\VSCode, HKCR\Drive\shell\VSCode) do reg delete %%i /f
 
 pushd %userprofile%\.vscode\extensions\ms-python.python-* && fart pythonFiles\lib\python\debugpy\_vendored\pydevd\_pydevd_bundle\pydevd_resolver.py "MAX_ITEMS_TO_HANDLE = 300" "MAX_ITEMS_TO_HANDLE = 4000"
 
