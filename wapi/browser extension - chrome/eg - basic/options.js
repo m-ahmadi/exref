bd1a0b72709d5ca90440c891b68fd4e954b401ce
m-ahmadi <ahmdee18@gmail.com>
2022-08-02 22:41:15 +0430

    1002

--- wapi/browser extension - chrome/eg - basic/options.js
+++ wapi/browser extension - chrome/eg - basic/options.js
@@ -1,6 +1,6 @@
-let page = document.getElementById("buttonDiv");
-let selectedClassName = "current";
-const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"];
+let page = document.getElementById('buttonDiv');
+let selectedClassName = 'current';
+const presetButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
 
 // Reacts to a button click by marking marking the selected button and saving
 // the selection
@@ -21,23 +21,23 @@ function handleButtonClick(event) {
 
 // Add a button to the page for each supplied color
 function constructOptions(buttonColors) {
-  chrome.storage.sync.get("color", (data) => {
+  chrome.storage.sync.get('color', (data) => {
     let currentColor = data.color;
 
     // For each color we were provided…
     for (let buttonColor of buttonColors) {
-      // …crate a button with that color…
-      let button = document.createElement("button");
+      // ...crate a button with that color…
+      let button = document.createElement('button');
       button.dataset.color = buttonColor;
       button.style.backgroundColor = buttonColor;
 
-      // …mark the currently selected color…
+      // ...mark the currently selected color…
       if (buttonColor === currentColor) {
         button.classList.add(selectedClassName);
       }
 
-      // …and register a listener for when that button is clicked
-      button.addEventListener("click", handleButtonClick);
+      // ...and register a listener for when that button is clicked
+      button.addEventListener('click', handleButtonClick);
       page.appendChild(button);
     }
   });
