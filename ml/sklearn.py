3e48009ff08041e35ab87ebcc579a40f31c3c025
m-ahmadi <ahmdee18@gmail.com>
2022-08-13 09:32:51 +0430

    1018

--- ml/sklearn.py
+++ ml/sklearn.py
@@ -87,6 +87,33 @@ sklearn.utils.class_weight.compute_class_weight(class_weight={}|'balanced'|None,
 from sklearn.utils.class_weight import compute_class_weight
 weights = compute_class_weight('balanced', classes=np.unique(y_train), y=y_org)
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# metrics
+
+#————————————————————————————————————————————————
+# roc_curve
+# https://scikit-learn.org/stable/modules/generated/sklearn.metrics.roc_curve.html
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
+
+# perfect classifier
+fpr, tpr, _ = roc_curve([1,1,1,1,0,0,0,0], [1,1,1,1,0,0,0,0])
+
+# 1 false positive (fpr=1/4=0.25, tpr=4/4=1)
+fpr, tpr, _ = roc_curve([1,1,1,1,0,0,0,0], [1,1,1,1,0,0,1,0])
+plt.plot(fpr, tpr)
+plt.show()
+
+# 1 false negative (fpr=0/4=0, tpr=3/4=0.75)
+fpr, tpr, _ = roc_curve([1,1,1,1,0,0,0,0], [1,0,1,1,0,0,0,0])
+plt.plot(fpr, tpr)
+plt.show()
+#————————————————————————————————————————————————
+# auc
+# https://scikit-learn.org/stable/modules/generated/sklearn.metrics.auc.html
+
+fpr, tpr, _ = roc_curve([1, 1, 2, 2], [0.1, 0.4, 0.35, 0.8], pos_label=2)
+auc(fpr, tpr)
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 # decision tree classifier
 # https://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html
 from sklearn.tree import DecisionTreeClassifier
