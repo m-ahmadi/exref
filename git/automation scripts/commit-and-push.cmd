895cf8f34c47eb9b25f4bb9af0af80ccc88eac5e
m-ahmadi <ahmdee18@gmail.com>
2022-08-17 14:15:05 +0430

    1029

--- git/automation scripts/commit-and-push.cmd
+++ git/automation scripts/commit-and-push.cmd
@@ -1,5 +1,9 @@
 @echo off
 cd ../../
+if exist bookmarks_*.html (
+	del bookmarks.html
+	rename bookmarks_*.html bookmarks.html
+)
 git log -1 --pretty=%%B > ttmmpp.txt
 set /p last=<ttmmpp.txt
 del ttmmpp.txt
