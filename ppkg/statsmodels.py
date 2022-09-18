f79dd64ff5cb51cb7f13494d021d4d60bcc467ca
m-ahmadi <ahmdee18@gmail.com>
2022-09-18 17:48:54 +0430

    1068

--- ppkg/statsmodels.py
+++ ppkg/statsmodels.py
@@ -12,3 +12,8 @@ rline = list(res.intercept + res.slope * x)
 df = pd.DataFrame({'x': x, 'y': y})
 model = ols('y ~ x', data=df).fit()
 print(list(model.resid)) # [-0.6, 0.79, 0.19, -0.4]
+
+# augmented dickey-fuller test
+from statsmodels.tsa.stattools import adfuller
+res = adfuller(x=[], maxlag=None|0, regression='c|ct|ctt|n', autolag='AIC|BIC|t-stat'|None, store=False, regresults=False)
+adf, pvalue, usedlag, nobs, critical_values, icbest, resstore = res
