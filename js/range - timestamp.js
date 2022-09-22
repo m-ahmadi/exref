b08c50215f6861c3861b716dd8a7f5db6a13ed34
m-ahmadi <ahmdee18@gmail.com>
2022-09-22 08:47:21 +0330

    1073

--- js/range - timestamp.js
+++ js/range - timestamp.js
@@ -17,3 +17,12 @@ d.getMilliseconds() // milliseconds
 
 // date strs (consistent form)
 [...Array(n)].map((d,i)=> (d=new Date, d.setDate(d.getDate()-n+i), d=d.getFullYear()*10000 + (d.getMonth()+1)*100 + d.getDate() + '', [d.slice(0,4),d.slice(4,6),d.slice(6,8)].join('-') ))
+
+// fixed-width (8days) decreasing range pairs
+let t = [];
+for (let i=0; i<10; i++) {
+  let a = t.length === 0 ? new Date() : t[t.length-1][1];
+  let b = new Date(+a);
+  b.setDate(b.getDate()-8);
+  t.push([a,b]);
+}
