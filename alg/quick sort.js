4ed722acd4b9f9895a240638b0b2938f7ae2f2be
m-ahmadi <ahmdee18@gmail.com>
2022-09-25 11:16:00 +0330

    1075

--- alg/quick sort.js
+++ alg/quick sort.js
@@ -8,7 +8,7 @@ function quickSort(origArray) {
 		let pivot = origArray.pop();
 		let length = origArray.length;
 		
-		for (let i=0; i < length; i++) {
+		for (let i=0; i<length; i++) {
 			if (origArray[i] <= pivot) {
 				left.push(origArray[i]);
 			} else {
@@ -24,23 +24,19 @@ quickSort([3, 0, 2, 5, -1, 4, 1 ]) // [-1, 0, 1, 2, 3, 4, 5]
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // another implementation
 function swap(items, leftIndex, rightIndex){
-	var temp = items[leftIndex];
+	let temp = items[leftIndex];
 	items[leftIndex] = items[rightIndex];
 	items[rightIndex] = temp;
 }
 function partition(items, left, right) {
-	var pivot   = items[Math.floor((right + left) / 2)], //middle element
-	i       = left, //left pointer
-	j       = right; //right pointer
+	let pivot = items[Math.floor((right + left) / 2)]; // middle element
+	let i = left;  // left pointer
+	let j = right; // right pointer
 	while (i <= j) {
-		while (items[i] < pivot) {
-			i++;
-		}
-		while (items[j] > pivot) {
-			j--;
-		}
+		while (items[i] < pivot) i++;
+		while (items[j] > pivot) j--;
 		if (i <= j) {
-			swap(items, i, j); //sawpping two elements
+			swap(items, i, j); // sawpping two elements
 			i++;
 			j--;
 		}
@@ -49,7 +45,7 @@ function partition(items, left, right) {
 }
 
 function quickSort(items, left, right) {
-	var index;
+	let index;
 	if (items.length > 1) {
 		index = partition(items, left, right); //index returned from partition
 		if (left < index - 1) { //more elements on the left side of the pivot
