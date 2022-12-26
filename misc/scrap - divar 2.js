38a33b755f62c33c0a7b60afd5935c04a6cca9ed
m-ahmadi <ahmdee18@gmail.com>
2022-12-26 12:22:03 +0330

    1200

--- misc/scrap - divar 2.js
+++ misc/scrap - divar 2.js
@@ -9,6 +9,7 @@ MAX_CREDIT = 450;
 COLUMNS        = ['convcredit', 'title', 'when', 'hood', 'singlefloor', 'stove', 'floor', 'parking', 'elevator', 'sqmeter', 'builtyear', 'rooms', 'storage', 'url'];
 COLUMN_HEADERS = ['ودیعه', 'عنوان', 'زمان', 'محل', 'تک‌واحدی', 'گازرومیزی', 'طبقه', 'پارکینگ', 'آسانسور', 'متراژ', 'سال‌ساخت', 'اتاق', 'انباری', 'لینک'];
 COLUMN_SORTS   = [  ['گازرومیزی'], ['زمان',1], ['تک‌واحدی'], ['آسانسور'], ['پارکینگ'],  ]; // [header, numericalSort, descSort]
+COLUMN_FILTERS = []; // [col, eq, val]
 
 MAX_ITEMS = Infinity;
 WAIT_AFTER_EACH_SCROLL = 1000;
@@ -204,8 +205,18 @@ for (let [idx, text] of texts.entries()) {
 	rr.push(row);
 }
 
+
+COLUMN_FILTERS.forEach(([col, eq, val]) => {
+	let j = COLUMN_HEADERS.indexOf(col);
+	if (eq) {
+		rr = rr.filter(i => i[j] === val);
+	} else {
+		rr = rr.filter(i => i[j] !== val);
+	}
+});
+
 if (MAKE_CSV) {
-	COLUMN_SORTS.map(([header, numericalSort, descSort]) => {
+	COLUMN_SORTS.forEach(([header, numericalSort, descSort]) => {
 		let j = COLUMN_HEADERS.indexOf(header);
 		if (numericalSort) {
 			descSort
