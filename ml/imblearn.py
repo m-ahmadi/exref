525fd26710790910b714e9af945f2a22c579545a
m-ahmadi <ahmdee18@gmail.com>
2023-03-19 20:40:48 +0330

    1295

--- ml/imblearn.py
+++ ml/imblearn.py
@@ -1,5 +1,24 @@
 # pip install imbalanced-learn
+
 from imblearn.ensemble import BalancedBaggingClassifier
+# https://imbalanced-learn.org/stable/references/generated/imblearn.ensemble.BalancedBaggingClassifier.html
+BalancedBaggingClassifier(
+	estimator=None|Estimator,
+	n_estimators=10, *,
+	max_samples=1.0|0,
+	max_features=1.0|0,
+	bootstrap=True,
+	bootstrap_features=False,
+	oob_score=False,
+	warm_start=False,
+	sampling_strategy='auto'|0.|{}|fn,
+	replacement=False,
+	n_jobs=None|0,
+	random_state=None|RandomState|0,
+	verbose=0,
+	sampler=None|Sampler,
+	base_estimator=None|Estimator # v0.10 deprecated (v0.12 remove)
+)
 
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
@@ -7,7 +26,7 @@ from sklearn.metrics import confusion_matrix
 
 from collections import Counter
 
-X, y = make_classification(
+x, y = make_classification(
 	n_classes=2,
 	class_sep=2,
 	weights=[0.1, 0.9],
@@ -17,15 +36,13 @@ X, y = make_classification(
 	n_features=20,
 	n_clusters_per_class=1,
 	n_samples=1000,
-	random_state=10
-)
+	random_state=10)
 
 print('Original dataset shape %s' % Counter(y))
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0)
 
 bbc = BalancedBaggingClassifier(random_state=42)
-bbc.fit(X_train, y_train) 
+bbc.fit(x_train, y_train) 
 
-y_pred = bbc.predict(X_test)
+y_pred = bbc.predict(x_test)
 print(confusion_matrix(y_test, y_pred))
