fec1f9dffd36e309dd79f7aaa1af11ea1990e4af
m-ahmadi <ahmdee18@gmail.com>
2023-03-23 07:38:45 +0430

    1306

--- cmd/batch/if else.bat
+++ cmd/batch/if else.bat
@@ -3,16 +3,28 @@ set var=Hello, World!
 
 if exist foo.txt echo The file was found.
 
-if a==a echo a equal a
-if not a==b echo a not equal b
+if %a%==%b% echo a equal b
+if not %a%==%b% echo a not equal b
 
+REM single-statement blocks
 if "%var%"=="Hello, World!" (
 	echo found
 ) else (
 	echo not found
 )
 
-:: default arg
+REM multi-statement blocks
+if %ERRORLEVEL%==0 goto :trueblock
+goto :endif
+:trueblock
+set var1=foo
+set var2=bar
+set var3=%var1%_%var2%
+:endif
+echo %var3%
+
+
+REM default arg example
 set arg=%1
 if "%1"=="" set arg=default
 echo %arg%
--- git/automation scripts/commit-and-push-if-changed.cmd
+++ git/automation scripts/commit-and-push-if-changed.cmd
@@ -1,9 +1,12 @@
 @echo off
-cd ../../
-if exist bookmarks_*.html (
-	del bookmarks.html
-	rename bookmarks_*.html bookmarks.html
-)
+
+set changed=0
+for /f %%i in ('git status --porcelain') do set changed=1
+
+if %changed%==1 goto :trueblock
+goto :endif
+
+:trueblock
 git log -1 --pretty=%%B > ttmmpp.txt
 set /p last=<ttmmpp.txt
 del ttmmpp.txt
@@ -11,3 +14,7 @@ set /a "new=%last%+1"
 git add .
 git commit -m "%new%"
 git push
+
+:endif
+
+pause
