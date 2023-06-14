80395c0a6b67bd4b2a9f80fed12ed94a55b696c7
m-ahmadi <ahmdee18@gmail.com>
2023-06-14 20:20:16 +0330

    1360

--- win/script/noiz.cmd
+++ win/script/noiz.cmd
@@ -1,10 +1,13 @@
 @echo off
+start /d "C:\Program Files\VideoLAN\VLC" vlc.exe "c:\dev\# General\noise\bass-1-pat20-space1.mp3"
+start /d "C:\Program Files\VideoLAN\VLC" vlc.exe "c:\dev\# General\noise\bass-2.mp3"
 start /d "C:\Program Files\VideoLAN\VLC" vlc.exe "c:\dev\# General\noise\noise-brown-3.mp3"
 start /d "C:\Program Files\VideoLAN\VLC" vlc.exe "c:\dev\# General\noise\noise-white.mp3"
 start /d "C:\Program Files\VideoLAN\VLC" vlc.exe "c:\dev\# General\noise\wave-1.mp3"
 start /d "C:\Program Files\VideoLAN\VLC" vlc.exe "c:\dev\# General\noise\wave-2.mp3"
 start /d "C:\Program Files\VideoLAN\VLC" vlc.exe "c:\dev\# General\noise\muz-3.mp3"
 
+REM max=2  top=829
 REM max=3  top=724
 REM max=4  top=620
 REM max=6  top=410
