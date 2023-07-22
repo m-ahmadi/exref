f5d88deb3e6dd4479d5231299698c461acd5ee6a
m-ahmadi <ahmdee18@gmail.com>
2023-07-23 03:22:01 +0330

    1384

--- js/es6+/async - sol - simple multi req.js
+++ js/es6+/async - sol - simple multi req.js
@@ -62,3 +62,68 @@ while (proms.some(i=> i.status !== 200)) {
 
 let posts = await Promise.all( proms.map(i=> i.json()) );
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// infinite retry until all done
+// (turn network off mid-operation to see)
+WAIT_AFTER_EACH_REQUEST = 100;
+WAIT_AFTER_EACH_RETRY_PASS = 2000;
+sleep = ms => new Promise(r=> setTimeout(r,ms));
+
+let urls = [...Array(50)].map((_,i) => 'https://jsonplaceholder.typicode.com/posts/'+(i+1));
+let remains = new Map(urls.map(i => [i, undefined]));
+let tot = remains.size;
+let posts = [];
+
+while (remains.size > 0) {
+	for (let url of remains.keys()) {
+		let err = 0;
+		let spoil = () => err = 1;
+		
+		let r = await fetch(url).catch(spoil);
+		if (err) continue;
+		
+		post = await r.json().catch(spoil);
+		if (err) continue;
+		
+		remains.delete(url);
+		posts.push(post);
+		
+		console.clear();
+		console.log(tot-remains.size+'/'+tot);
+		
+		await sleep(WAIT_AFTER_EACH_REQUEST);
+	}
+	await sleep(WAIT_AFTER_EACH_RETRY_PASS);
+}
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// infinite retry until all done - wait til all settled at each retry pass
+WAIT_AFTER_EACH_REQUEST = 100;
+sleep = ms => new Promise(r=> setTimeout(r,ms));
+
+let urls = [...Array(50)].map((_,i) => 'https://jsonplaceholder.typicode.com/posts/'+(i+1));
+let remains = new Map(urls.map(i=> [i, undefined]));
+let tot = remains.size;
+let posts = [];
+
+while (remains.size > 0) {
+	let proms = [];
+	
+	for (let url of remains.keys()) {
+		let prom = fetch(url).then(resp => {
+			if (resp.status === 200) {
+				remains.delete(url);
+				resp.json().then(post => {
+					posts.push(post);
+				});
+			} else if (resp.status === 404) {
+				remains.delete(url);
+			}
+			console.clear();
+			console.log(tot-remains.size+'/'+tot);
+		}).catch(()=> undefined);
+		
+		proms.push(prom);
+		await sleep(WAIT_AFTER_EACH_REQUEST);
+	}
+	await Promise.allSettled(proms);
+}
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
