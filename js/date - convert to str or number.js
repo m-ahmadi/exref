4163fead24bd352b20ed1a5ebfee17ccd7667984
m-ahmadi <ahmdee18@gmail.com>
2023-07-28 23:04:21 +0330

    1391

--- js/date - convert to str or number.js
+++ js/date - convert to str or number.js
@@ -1,5 +1,5 @@
 let d = new Date();
 
-(d.getFullYear()*10000) + ( (d.getMonth()+1)*100 ) + d.getDate() + '' // 20210724
+d.getFullYear()*10000 + (d.getMonth()+1)*100 + d.getDate() + '' // 20210724
 
 [d.getFullYear(), d.getMonth()+1, d.getDate()].map(i=>(i=''+i, i.length>1?i:'0'+i)).join('-'); // '2021-07-24'
