b5fde65394fa05a6d77f564172c2f717d6857f93
m-ahmadi <ahmdee18@gmail.com>
2023-08-08 10:16:03 +0330

    1403

--- js/range - timestamp.js
+++ js/range - timestamp.js
@@ -18,11 +18,31 @@ d.getMilliseconds() // milliseconds
 // date strs (consistent form)
 [...Array(n)].map((d,i)=> (d=new Date, d.setDate(d.getDate()-n+i), d=d.getFullYear()*10000 + (d.getMonth()+1)*100 + d.getDate() + '', [d.slice(0,4),d.slice(4,6),d.slice(6,8)].join('-') ))
 
-// fixed-width (8days) decreasing range pairs
-let t = [];
+// 8day decreasing date pairs (10 pairs starting from now)
+let r = [];
 for (let i=0; i<10; i++) {
-  let a = t.length === 0 ? new Date() : t[t.length-1][1];
-  let b = new Date(+a);
-  b.setDate(b.getDate()-8);
-  t.push([a,b]);
+  let d0 = r.length === 0 ? new Date() : r[r.length-1][1];
+  let d1 = new Date(d0);
+  d1.setDate(d1.getDate() - 8);
+  r.push([d0, d1]);
+}
+
+// monthly increasing date pairs (from start date till now)
+let d = new Date(2002,0,1);
+let now = +new Date();
+let r = [];
+while (+d <= now) {
+  let d0 = new Date(d);
+  d.setMonth(d.getMonth() + 1);
+	let d1 = new Date(d);
+  r.push([d0, d1]);
+}
+
+// monthly intervals from a start date till now
+let d = new Date(2002,0,1);
+let now = +new Date();
+let r = [];
+while (+d <= now) {
+  r.push(new Date(d));
+  d.setMonth(d.getMonth() + 1);
 }
