dd8b86a423b23771633fcdd9a805cbe32a4812ba
m-ahmadi <ahmdee18@gmail.com>
2023-08-08 10:16:03 +0330

    1403

--- js/date.js
+++ js/date.js
@@ -1,23 +1,23 @@
-new Date()
-new Date(value)
-new Date(dateString)
-new Date(year, monthIndex [, day [, hours [, minutes [, seconds [, milliseconds]]]]])
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/Date
+new Date(?value=int timestamp|string date|Date date|int year, ?monthIndex=int, ?day=int, ?hours=int, ?minutes=int, ?seconds=int, ?milliseconds=int)
 
 /* NOTE:
 don't parse date strings. (whether with constructor or parse() method)
 use numbers instead.
 why: because of browser differences and inconsistencies. */
 
-Date()                 // returns string (not Date object)
-new Date()             // current time
-new Date('2019/10/09') // parses date string
-new Date('2019,10,09') // same as above
-new Date('2019-10-09') // treated as utc
-new Date(2019, 9, 09)  // best way. monthIndex= month - 1 (3= 4th month)
+Date()                  // returns string (not Date object)
+new Date()              // current time
+new Date(2014, 9, 30)   // best way. monthIndex = month - 1 (3 = 4th month)
+new Date(1414614600000) // from timestamp
+new Date('2014/10/30')  // parses date string
+new Date('2014,10,30')  // same as above
+new Date('2014-10-30')  // treated as utc
 
-// missing args get lowest possible value: (1 for day and 0 for rest)
-new Date(2019)         // 2019 01 01
-new Date(2019, 11)     // 2019 12 01
+// missing args get lowest possible value (1 for day and 0 for rest)
+new Date(2014)          // err (expects timestamp)
+new Date(2014, 11)      // 2014 12 01
+new Date(2014, 11, 5)   // 2014 12 05 00:00:00
 
 // static methods
 Date.now()
@@ -27,13 +27,24 @@ Date.parse('2016/4/20') === new Date('2016/4/20').getTime() // true
 
 // instance methods
 var date = new Date(unix_timestamp*1000)
-date.getTime()         // timestamp
-date.getFullYear()     // year (4 digits for 4-digit years). 2016
-date.getYear()         // year (2-3 digits). 116 (don't use)
-date.getMonth()        // month index (0-11)
-date.getDate()         // day of the month
-date.getDay()          // day of the week (0-6)
-date.getHours()        // hour (0-23)
-date.getMinutes()      // minutes (0-59)
-date.getSeconds()      // seconds (0-59)
-date.getMilliseconds() // milliseconds (0-999)
+date.getTime()          // timestamp
+date.getFullYear()      // year (4 digits for 4-digit years). 2016
+date.getYear()          // year (2-3 digits). 116 (don't use)
+date.getMonth()         // month index (0-11)
+date.getDate()          // day of the month
+date.getDay()           // day of the week (0-6)
+date.getHours()         // hour (0-23)
+date.getMinutes()       // minutes (0-59)
+date.getSeconds()       // seconds (0-59)
+date.getMilliseconds()  // milliseconds (0-999)
+
+// pass-by-reference
+var date = new Date(2010,0,1);
+var tmp = date;
+date.setFullYear(2014);
+tmp.getFullYear()       // 2014
+
+var date = new Date(2010,0,1);
+var tmp = new Date(date);
+date.setFullYear(2014);
+tmp.getFullYear()       // 2010
--- js/rand - color.js
+++ js/rand - color.js
@@ -5,39 +5,36 @@
 '#' + (Math.random() * 0xFFFFFF << 0).toString(16)
 
 function getRandColor(brightness) {
-	// Six levels of brightness from 0 to 5, 0 being the darkest
-	var rgb = [Math.random() * 256, Math.random() * 256, Math.random() * 256];
-	var mix = [brightness * 51, brightness * 51, brightness * 51]; //51 => 255/5
-	var mixedrgb = [rgb[0] + mix[0], rgb[1] + mix[1], rgb[2] + mix[2]].map(function (x) {
-		return Math.round(x / 2.0)
-	})
+	// 6 levels of brightness from 0 to 5 (0 == darkest)
+	let rgb = [Math.random() * 256, Math.random() * 256, Math.random() * 256];
+	let mix = [brightness * 51, brightness * 51, brightness * 51]; // 51 => 255/5
+	let mixedrgb = [rgb[0] + mix[0], rgb[1] + mix[1], rgb[2] + mix[2]].map(x => Math.round(x / 2.0));
 	return 'rgb(' + mixedrgb.join(',') + ')';
 }
 
 function getRandomColor() {
-	var letters = '0123456789ABCDEF';
-	var color = '#';
-	var i;
+	let letters = '0123456789ABCDEF';
+	let color = '#';
+	let i;
 	for (i=0; i<6; i++) {
 		color += letters[Math.floor(Math.random() * 16)];
 	}
 	return color;
 }
 
-function get_random_color() {
+function getRandomColor() {
 	function c() {
-		var hex = Math.floor(Math.random() * 256).toString(16);
+		let hex = Math.floor(Math.random() * 256).toString(16);
 		return ('0' + String(hex)).substr(-2); // pad with zero
 	}
 	return '#' + c() + c() + c();
 }
 
-function get_random_color() {
-	// I needed to generate only light colours (for backgrounds), so I went with three letter (#AAA) format:
-	var letters = 'ABCDE'.split('');
-	var color = '#';
-	var i;
-	for (i=0; i<3; i++) {
+function getRandomColor() {
+	// I needed to generate only light colors (for backgrounds), so I went with three letter (#AAA) format:
+	let letters = 'ABCDE'.split('');
+	let color = '#';
+	for (let i=0; i<3; i++) {
 		color += letters[Math.floor(Math.random() * letters.length)];
 	}
 	return color;
