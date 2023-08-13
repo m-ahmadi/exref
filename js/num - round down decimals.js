afd486bd8f779be94f279998e5c0ff43b69f9892
m-ahmadi <ahmdee18@gmail.com>
2023-08-13 18:56:30 +0330

    1437

--- js/num - round down decimals.js
+++ js/num - round down decimals.js
@@ -1,23 +1,38 @@
-rd1(1.025, 2)    // 1.02
-rd1(-1.025, 2)   // -1.02
-rd1(4.56, 2)     // 4.56
-rd1(42.464)      // 42
-rd1(1.12496, 3)  // 1.124
-rd1(221.245, -1) // 220
-rd1(220.245, -1) // 220
-rd1(219.245, -1) // 210
-
-// big.js (better)
-Big.RM = 0;
-function rd1(n, d=0) {
-	return parseFloat( Big(n).round(d).toString() );
-}
-
-// has issues
-function rd2(num, decimals=0) {
+function rd(num, decimals=0) {
 	const n = Math.pow(10, decimals);
-	return Math.floor(num * n) / n;
+	return Math.round(num * n) / n;
 }
-// issues:
-rd2(-1.025, 2)   // -1.03
-rd2(4.56, 2)     // 4.56
+rd(1.234, 2)  // 1.23
+rd(1.235, 2)  // 1.24
+rd(-1.234, 2) // -1.23
+rd(224, -1)   // 220
+rd(225, -1)   // 230
+rd(240, -2)   // 200
+rd(250, -2)   // 300
+rd(400, -3)   // 0
+rd(500, -3)   // 1000
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// big.js
+// https://mikemcl.github.io/big.js/#rm
+
+function rd2(n, d=0) {
+	return Big(n).round(d).toNumber();
+}
+
+// same as above
+rd2(-1.234, 2) // -1.23
+rd2(250, -2)   // 300
+// ...
+
+// cutoff point = 5, regardless of odd|even neighbor
+Big.RM = 1;
+rd2(1.224, 2) // 1.22 (even neighbor)
+rd2(1.225, 2) // 1.23 ...
+rd2(1.235, 2) // 1.24 (odd neighbor)
+
+// cutoff point = 6, but only when neighbor is even
+Big.RM = 2;
+rd2(1.225, 2) // 1.22 (even neighbor)
+rd2(1.226, 2) // 1.23 ...
+rd2(1.235, 2) // 1.24 (odd neighbor)
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
