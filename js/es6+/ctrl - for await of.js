a928693e20e3d5c390b6bc96dd04e1f16918e115
m-ahmadi <ahmdee18@gmail.com>
2023-09-06 06:28:57 +0330

    1467

--- js/es6+/ctrl - for await of.js
+++ js/es6+/ctrl - for await of.js
@@ -1,27 +1,42 @@
-for await (variable of iterable) {
-  statement
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for-await...of
+
+async function* foo() {
+	yield 1;
+	yield 2;
 }
-// iterates over sync/async iterable
+for await (const num of foo()) console.log(num);
 
 
-for (let value of iterable) {
-	console.log(i);
+
+function* f () {
+	const url = 'https://jsonplaceholder.typicode.com/users/';
+	yield fetch(url+1);
+	yield fetch(url+2);
+	yield fetch(url+3);
 }
+for await (const r of f()) {
+	const user = await r.json();
+	console.log(user.name);
+}
+
+
 
 async function* asyncGenerator() {
-  let i = 0;
-  while (i < 3) {
+	let i = 0;
+	while (i < 3) {
 		await new Promise(r => setTimeout(r, 1000));
 		yield i++;
 	}
 }
 for await (let num of asyncGenerator()) console.log(num);
 
+
+
 function* generator() {
-  yield 0;
-  yield 1;
-  yield Promise.resolve(2);
-  yield Promise.resolve(3);
-  yield 4;
+	yield 0;
+	yield 1;
+	yield Promise.resolve(2);
+	yield Promise.resolve(3);
+	yield 4;
 }
 for await (let num of generator()) console.log(num);
--- js/es6+/ctrl - for of.js
+++ js/es6+/ctrl - for of.js
@@ -1,9 +1,7 @@
-for (variable of iterable) {
-	statement
-}
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
 
 var iterable = [10, 20, 30];
-// for of can iterate over iterable objects. (Array, Map, Set, String, TypedArray, arguments object, ...)
+// for...of can iterate over iterable objects. (Array, Map, Set, String, TypedArray, arguments object, ...)
 for (let value of iterable) {
 	value += 1;
 	console.log(value);
@@ -16,7 +14,7 @@ for (const value of arr) {
 }
 // 1, 'string', false
 
-// continue break same as for
+// continue & break same as for
 for (var i of [1,2,3,4,5,6]) {
 	if (i < 2) continue;
 	if (i > 4) break;
@@ -28,17 +26,17 @@ for (const [i, v] of ['a', 'b', 'c'].entries()) {
 	console.log(i, v)
 }
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-// for of vs for in
+// for...of vs for...in
 
 var list = [4, 5, 6];
 list.foo = 'hello';
 
-// for of iterates over values
+// for...of iterates over values
 for (const i of list) {
 	console.log(i); // 4, 5, 6
 }
 
-// for in iterates over keys
+// for...in iterates over keys
 for (const i in list) {
 	console.log(i);   // 0, 1, 2
 }
