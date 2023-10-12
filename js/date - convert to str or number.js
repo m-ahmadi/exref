a729729ebb65b1ae54db1ab9c178ff849b76feec
m-ahmadi <ahmdee18@gmail.com>
2023-10-12 04:39:00 +0330

    1476

--- js/date - convert to str or number.js
+++ js/date - convert to str or number.js
@@ -2,5 +2,7 @@ let d = new Date(2014,0,1);
 
 d.getFullYear()*10000 + (d.getMonth()+1)*100 + d.getDate() + '' // '20140101'
 
+Intl.DateTimeFormat().format(d).split('/').reverse().join('') // '20140101'
+
 [d.getFullYear(), d.getMonth()+1, d.getDate()].map(i=>i<10?'0'+i:i).join('-')                  // '2014-01-01'
 [d.getFullYear(), d.getMonth()+1, d.getDate()].map(i=>(i=''+i, i.length>1?i:'0'+i)).join('-'); // '2014-01-01'
