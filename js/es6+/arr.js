793a31c7f5afb00add02b3504e33af8d9b3057d8
m-ahmadi <ahmdee18@gmail.com>
2023-12-07 18:04:13 +0330

    1500

--- js/es6+/arr.js
+++ js/es6+/arr.js
@@ -1,6 +1,15 @@
+arr.at(index)
 arr.toSorted(?compareFn=(a,b)=>)
 arr.toSpliced(start, ?deleteCount, ?...items)
 arr.toReversed()
+arr.with(index, value)
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// examples
+
+a = [1,2,3,4,5];
+a.at(2)  // 3
+a.at(-1) // 4
 
 a = [3,2,1];
 b = a.toSorted();
@@ -13,3 +22,6 @@ b // [1, 44, 2, 3]
 a = [1,2,3];
 b = a.toReversed();
 b// [3,2,1]
+
+a = [1,2,3];
+a.with(1, 'hi') // [1, 'hi', 3]
