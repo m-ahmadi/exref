1de6757cef2649104c1edbc412d56bd53f2bd3e4
m-ahmadi <ahmdee18@gmail.com>
2024-01-17 01:51:43 +0330

    1520

--- js/console - util.js
+++ js/console - util.js
@@ -1,10 +1,11 @@
-$0	// last selected element in elements tab
-$1	// 2nd last
-$2	// 3rd last
-$3	// 4th last
-$4	// 5th last
+$0  // last selected element in elements tab
+$1  // 2nd last
+$2  // 3rd last
+$3  // 4th last
+$4  // 5th last
 
-$_ // most recently evaluated expression
+$_  // last evaluated expression
 
 $(selector, ?startNode)  // querySelector()
 $$(selector, ?startNode) // querySelectorAll()
+$x(xPathSelector)        // https://devhints.io/xpath
