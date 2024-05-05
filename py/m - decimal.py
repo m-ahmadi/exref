3768dbdc8d110df5e454df7a783304263542f7fd
m-ahmadi <ahmdee18@gmail.com>
2024-05-05 18:57:26 +0330

    1569

--- py/m - decimal.py
+++ py/m - decimal.py
@@ -28,3 +28,16 @@ Decimal('7.325').quantize(Decimal(10)**-2, rounding=rm) # ...
 
 Decimal(10) ** -0 # Decimal('1')
 Decimal(10) ** -2 # Decimal('0.01')
+
+# set defaults - decimal places (on the fly)
+from decimal import setcontext, Context
+Decimal('0.123456789123456789')*1 # '0.123456789123456789'
+setcontext(Context(prec=4))
+Decimal('0.123456789123456789')*1 # '0.1235'
+
+# set defaults - application-wide (for all threads)
+from decimal import setcontext, DefaultContext
+DefaultContext.prec = 4
+DefaultContext.rounding = decimal.ROUND_HALF_EVEN
+setcontext(DefaultContext)
+Decimal('0.123456789123456789')*1 # # '0.1235'
