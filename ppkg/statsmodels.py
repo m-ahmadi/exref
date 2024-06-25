e818199810291e225b0f8a1105928058ba1c2edc
m-ahmadi <ahmdee18@gmail.com>
2024-06-25 23:19:29 +0330

    1676

--- ppkg/statsmodels.py
+++ ppkg/statsmodels.py
@@ -38,3 +38,59 @@ plot_acf(x, lags=49)
 plot_pacf(x, lags=25) # lags <= len(x)/2
 plt.show()
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# decompose time-series into components (trend, seasonality, error)
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+seasonal_decompose(x=[], model='additive|multiplicative', filt=None|[], period=None|0, two_sided=True, extrapolate_trend=0|'freq') -> DecomposeResult
+# https://www.statsmodels.org/stable/generated/statsmodels.tsa.seasonal.seasonal_decompose.html
+
+DecomposeResult(observed, seasonal, trend, resid, weights=None)
+	.nobs
+	.observed
+	.resid
+	.seasonal
+	.trend
+	.weights
+	.plot(observed=True, seasonal=True, trend=True, resid=True, weights=False) -> matplotlib.figure.Figure
+# https://www.statsmodels.org/stable/generated/statsmodels.tsa.seasonal.DecomposeResult.html
+
+# example
+import numpy as np
+import datetime as dt
+import pandas as pd
+from statsmodels.tsa.seasonal import seasonal_decompose
+import matplotlib.pyplot as plt
+
+n = 250
+time_series = np.abs(np.cumsum(np.random.randn(n)))
+
+# with x df
+idxs = pd.DatetimeIndex([dt.datetime(2023,1,1) + dt.timedelta(days=i) for i in range(n)])
+x_df = pd.DataFrame(time_series, index=idxs, columns=['value'])
+
+# multiplicative decomposition
+res = seasonal_decompose(x_df, model='multiplicative', extrapolate_trend='freq')
+x_reconstructed = res.seasonal * res.trend * res.resid
+(x_df - x_reconstructed).sum().value  # ≈ 0
+x_df.equals(x_reconstructed.round(8)) # True
+res.plot()
+plt.show()
+
+# additive decomposition
+res = seasonal_decompose(x_df, model='additive', extrapolate_trend='freq')
+x_reconstructed = (res.seasonal + res.trend + res.resid).to_frame()
+x_reconstructed.columns = [*x_df.columns]
+(x_df - x_reconstructed).sum().value  # ≈ 0
+x_df.equals(x_reconstructed.round(8)) # True
+res.plot()
+plt.show()
+
+# with x array
+x_arr = time_series
+res = seasonal_decompose(x_arr, model='multiplicative', extrapolate_trend='freq', period=25)
+x_reconstructed = res.seasonal * res.trend * res.resid
+sum(x_arr - x_reconstructed)                       # ≈ 0
+np.all(x_arr.round(8) == x_reconstructed.round(8)) # True
+res.plot()
+plt.show()
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
