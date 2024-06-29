c2aea3c9fd1f8e306130885cd4fe28b6f00c8272
m-ahmadi <ahmdee18@gmail.com>
2024-06-29 07:11:02 +0330

    1683

--- ppkg/matplotlib/eg - subplots.py
+++ ppkg/matplotlib/eg - subplots.py
@@ -43,6 +43,11 @@ axs[1].plot(y, label='label2')
 for ax in axs:
 	ax.legend(loc='upper left')
 
+# ... figsize
+_, axs = plt.subplots(2, figsize=(16,8))
+axs[0].plot(x, y)
+axs[1].plot(x, -y)
+
 
 # stack two directions
 fig, axs = plt.subplots(2, 2)
@@ -121,14 +126,14 @@ for ax in axs.flat:
 # more complex sharing structure
 fig, axs = plt.subplots(2, 2)
 axs[0,0].plot(x, y)
-axs[0,0].set_title("main")
+axs[0,0].set_title('main')
 axs[1,0].plot(x, y**2)
-axs[1,0].set_title("shares x with main")
+axs[1,0].set_title('shares x with main')
 axs[1,0].sharex(axs[0, 0])
 axs[0,1].plot(x + 1, y + 1)
-axs[0,1].set_title("unrelated")
+axs[0,1].set_title('unrelated')
 axs[1,1].plot(x + 2, y + 2)
-axs[1,1].set_title("also unrelated")
+axs[1,1].set_title('also unrelated')
 fig.tight_layout()
 
 
