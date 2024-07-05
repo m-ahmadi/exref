34e9a09ce93ae201ae81fd84d57e08189f3e2ba6
m-ahmadi <ahmdee18@gmail.com>
2024-07-05 12:27:24 +0330

    1713

--- py/sol - print table.py
+++ py/sol - print table.py
@@ -8,12 +8,14 @@ def print_table(rows=[[]]):
 	cols_max = [max(cols[col]) for col in cols]
 	s = ''
 	for row in rows:
+		last_idx = len(row) - 1
 		for col_idx, col in enumerate(row):
 			col = str(col)
 			col_max = cols_max[col_idx]
 			char_len = len(col)
 			delta = ' ' * (col_max - char_len)
-			s += col + delta + '\t'
+			not_last = col_idx < last_idx
+			s += col + (delta if not_last else '') + '\t'
 		s = s[:-1]
 		s += '\n'
 	return s
@@ -30,10 +32,10 @@ table = [
 
 print(print_table(table))
 '''
-foo	bar   	baz 
-a  	b     	c   
-aa 	bb    	cc  
-x  	y     
+foo	bar   	baz
+a  	b     	c
+aa 	bb    	cc
+x  	y
 z  	yyyyyy
 ini	mini  	miny	moe
 '''
