c859ddfc4ab92255402b04b3d58d6504192cd12c
m-ahmadi <ahmdee18@gmail.com>
2024-07-31 12:02:15 +0330

    1784

--- tse/api - misc/IntraDayPrice.js
+++ tse/api - misc/IntraDayPrice.js
@@ -7,7 +7,7 @@ http://www.tsetmc.com/tsev2/chart/data/IntraDayPrice.aspx?
 	return: text/csv
 */
 
-var str = await fetch('http://www.tsetmc.com/tsev2/chart/data/IntraDayPrice.aspx?i=46348559193224090').then(async r => await r.text());
+var str = await (await fetch('http://www.tsetmc.com/tsev2/chart/data/IntraDayPrice.aspx?i=46348559193224090')).text();
 var rows = str.split(';').map(i=> i.split(','));
 
 var cols = ['time', 'open', 'high', 'low', 'close', 'volume'];
--- tse/api - misc/TradeDetail.js
+++ tse/api - misc/TradeDetail.js
@@ -5,7 +5,7 @@ http://www.tsetmc.com/tsev2/data/TradeDetail.aspx?
 	return: text/xml
 */
 
-var str = await fetch('http://www.tsetmc.com/tsev2/data/TradeDetail.aspx?i=46348559193224090').then(async r => await r.text());
+var str = await (await fetch('http://www.tsetmc.com/tsev2/data/TradeDetail.aspx?i=46348559193224090')).text();
 var rowsXml = new DOMParser().parseFromString(str, mimeType='text/xml').children[0];
 var rows = [...rowsXml.children].map(i=> [...i.children].map(j=> j.getInnerHTML() ));
 /*
