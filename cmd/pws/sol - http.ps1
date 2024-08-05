144232f19564b4534b2223aad40cbe381586a556
m-ahmadi <ahmdee18@gmail.com>
2024-08-05 10:12:49 +0330

    1792

--- cmd/pws/sol - http.ps1
+++ cmd/pws/sol - http.ps1
@@ -2,13 +2,15 @@ iwr https://jsonplaceholder.typicode.com/posts/1 -O f.json;
 iwr http://ipv4.download.thinkbroadband.com/5MB.zip -O f.zip
 
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-# more
-$url = 'https://jsonplaceholder.typicode.com/posts/1';
+# json
 
-$req = Invoke-WebRequest -Uri $url;
-$req.Content
+$res = Invoke-RestMethod -Uri 'https://jsonplaceholder.typicode.com/users/1'
+$res.name # 'Leanne Graham'
+$res | ft -Property name, email, website
 
-$res = Invoke-RestMethod -Uri $url;
-$res.title
-Invoke-RestMethod -Uri $url | ft -Property title, id
+# manual parse
+$resp = Invoke-WebRequest -Uri 'https://jsonplaceholder.typicode.com/users/3'
+$resp.Content.GetType().Name # String
+$res = ConvertFrom-Json $resp.Content
+$res.name # 'Clementine Bauch'
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
