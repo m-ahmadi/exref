0573fcb9a3d351be4bcff2e2d1ed0e6af0dcf096
m-ahmadi <ahmdee18@gmail.com>
2024-09-08 05:14:44 +0330

    1837

--- node/module - esm.js
+++ node/module - esm.js
@@ -1,3 +1,9 @@
+// https://nodejs.org/docs/latest/api/esm.html
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// enabling
+// https://nodejs.org/docs/latest/api/esm.html#enabling
+
 // index.mjs
 import x from './foo.mjs';
 console.log(x);
@@ -13,3 +19,16 @@ import x from './foo.js';
 console.log(x);
 // foo.js
 export default 'hello'
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// import built-in node modules
+import { readFile } from 'node:fs';
+import { readFileSync } from 'node:fs';
+import fs, { readFileSync } from 'node:fs';
+import EventEmitter from 'node:events';
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// __filename __dirname
+// https://nodejs.org/docs/latest/api/esm.html#no-__filename-or-__dirname
+
+console.log(import.meta.filename);
+console.log(import.meta.dirname);
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
