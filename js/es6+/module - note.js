168cacbb771c8553f891e6df28edd192ce589772
m-ahmadi <ahmdee18@gmail.com>
2024-09-17 13:28:25 +0330

    1847

--- js/es6+/module - note.js
+++ js/es6+/module - note.js
@@ -5,4 +5,11 @@ import * as m from './person';
 // named import: (person has to have a named export)
 import { person } from './person';
 
-// inside a module, top-level value of `this` is undefined.
+// inside a module, top-level value of `this` is undefined
+this // undefined
+
+// how to access global object in a cross-environment way (browser & node & web-worker)
+globalThis
+globalThis === global // true in node
+globalThis === window // true in browser
+globalThis === self   // true in web-worker
