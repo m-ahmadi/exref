9354424e1e2e113805e7bc7b960e734283677470
m-ahmadi <ahmdee18@gmail.com>
2024-09-18 08:26:03 +0330

    1848

---
+++ js/arch - statement vs declaration.js
@@ -0,0 +1,34 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements
+
+// declarations
+let
+const
+function
+function*
+async function
+async function*
+class
+export
+import
+
+// statements
+return
+break
+continue
+throw
+if...else
+switch
+try...catch
+
+do...while
+for
+for...in
+for...of
+for await...of
+while
+
+Empty
+Block
+Expression
+debugger
+with // deprecated
