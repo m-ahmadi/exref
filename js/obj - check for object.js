37254845c570a2f1355a1ade058120f9cdf08866
m-ahmadi <ahmdee18@gmail.com>
2024-09-25 17:05:55 +0330

    1856

--- js/obj - check for object.js
+++ js/obj - check for object.js
@@ -1,11 +1,17 @@
-Array.prototype.toString.call(v) === '[object Object]'
+Object.prototype.toString.call(v) === '[object Object]'
 
 // accept functions as objects
 v !== null && (typeof v === 'object' || typeof v === 'function')
 
 // don't accept arrays
-v !== null && typeof v === 'object' && Array.prototype.toString.call(v) !== '[object Array]' // !$.isArray(foo)
+v !== null && typeof v === 'object' && Object.prototype.toString.call(v) !== '[object Array]' // !$.isArray(foo)
 
 typeof {}     // 'object'
 typeof null   // 'object'
 null === null // true
+
+Object.prototype.toString.call({}) // '[object Object]'
+Object.prototype.toString.call([]) // '[object Array]'
+
+Array.prototype.toString.call({})  // '[object Object]'
+Array.prototype.toString.call([])  // ''
