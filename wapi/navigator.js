062cdcbfbb065563671104a563976c184bcff9d0
m-ahmadi <ahmdee18@gmail.com>
2024-09-27 20:32:26 +0330

    1858

---
+++ wapi/navigator.js
@@ -0,0 +1,6 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Navigator
+
+// default locale
+// how it is determined:  https://datatracker.ietf.org/doc/html/rfc5646
+navigator.language  // 'en-GB'  (on laptop is 'en-US')
+navigator.languages // ['en-GB', 'en-US', 'en', 'fa']
