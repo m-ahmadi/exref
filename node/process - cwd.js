07bc7dff65f548fb245ad27bda88ad959cab94f2
m-ahmadi <ahmdee18@gmail.com>
2024-10-07 17:06:47 +0330

    1868

--- node/process - cwd.js
+++ node/process - cwd.js
@@ -1,8 +1,8 @@
 process.cwd()
-// returns current working directory of the node.js process.
+// returns current working directory of the node.js process
 // (the directory from which you invoked the node command)
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-// .cwd() does not change when a required module is being executed:
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// cwd does not change when a required module is being executed:
 // cd home/htdocs/try/ && node a.js
 // a.js
 console.log( process.cwd() ) // home/htdocs/try
@@ -10,29 +10,44 @@ require('./b.js')
 console.log('see?')          // see?
 // b.js
 console.log( process.cwd() ) // home/htdocs/try
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-// changing it:
-process.cwd()          // 'C:\\Program Files\\nodejs'
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// changing cwd
+process.cwd()         // 'C:\\Program Files\\nodejs'
 process.chdir('../')
-process.cwd()          // 'C:\\Program Files'
+process.cwd()         // 'C:\\Program Files'
 process.chdir('d:')
-process.cwd()          // 'D:\\'
+process.cwd()         // 'D:\\'
 process.chdir('test')
-process.cwd()          // 'D:\\test'
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+process.cwd()         // 'D:\\test'
+
+// change cwd to script's dir
+process.chdir(__dirname)
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // process.cwd() vs __dirname
 process.cwd()
 // directory from which node was invoked.
-// equal to '.' (except in require() calls, since path inside require is always relative to the file calling it.)
-// does not change in different modules.
+// equal to '.' (except in require() calls, since path inside require is always relative to the file calling it)
+// does not change in different modules
 
 __dirname
-// dir name of current module.
-// changes in different modules.
+// directory of current module
+// changes in different modules
 
 // home/a.js
 console.log(__dirname) // home/
 require('./etc/b')
 // home/etc/b.js
 console.log(__dirname) // home/etc/
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// cwd can be changed by required scripts
+
+// index.js
+process.cwd(); // 'E:\xampp\htdocs\foo'
+require('./another')();
+process.cwd(); // 'E:\xampp\htdocs'
+
+// another.js
+module.exports = () => {
+	process.chdir('../');
+};
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
