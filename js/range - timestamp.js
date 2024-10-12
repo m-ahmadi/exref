22c503d7e71abc421f16d1c357204f53dfebc2c4
m-ahmadi <ahmdee18@gmail.com>
2024-10-12 07:33:38 +0330

    1880

--- js/range - timestamp.js
+++ js/range - timestamp.js
@@ -1,6 +1,6 @@
 // from n days ago till today
 let n = 400;
-[...Array(n)].map((d,i)=> (d=new Date, d.setDate(d.getDate()-n+i)/1000))
+[...Array(n)].map((d,i)=> (d=new Date(), d.setDate(d.getDate()-n+i)/1000))
 
 // other intervals
 d.getFullYear()     // year
