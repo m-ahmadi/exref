ade900efe9b08c8a9a940d9e2837cf1b0d46ace9
m-ahmadi <ahmdee18@gmail.com>
2024-10-16 10:32:21 +0330

    1898

--- node/readline.js
+++ node/readline.js
@@ -1,12 +1,26 @@
 const readline = require('readline');
+const {stdin: input, stdout: output} = process;
+const rl = readline.createInterface({input,output}); // once created, takes control
+// note: must run one example at a time
 
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+
+// one question
+rl.question('How are you? ', answer => {
+	console.log(`You are ${answer}`);
+	rl.close();
 });
 
-rl.question('What do you think of Node.js? ', answer => {
-	// TODO: Log the answer in a database
-	console.log(`Thank you for your valuable feedback: ${answer}`);
-	rl.close();
+
+// more questions
+rl.question('Tell me you name: ', name => {
+	rl.question('Tell me you date of birth as YYYY/MM/DD: ', dob => {
+		var [y,m,d] = dob.split('/').map(parseFloat);
+		var today = new Date();
+		var isBirthday = today.getMonth()+1 === m && today.getDate() === d;
+		var msg = isBirthday
+			? `Happy birthday dear ${name}`
+			: 'Sorry, today is not your birthday.';
+		console.log(msg);
+		rl.close();
+	});
 });
