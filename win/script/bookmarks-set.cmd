cb453e3f2d68e86f69130fb2c32145c24c26fe69
m-ahmadi <ahmdee18@gmail.com>
2024-11-20 16:44:39 +0330

    1971

--- win/script/bookmarks-set.cmd
+++ win/script/bookmarks-set.cmd
@@ -1,10 +1,26 @@
 @echo off
 set d=%LOCALAPPDATA%\Google\Chrome\User Data\Default
 
+echo Force closing chrome.exe processes...
 for /f "tokens=2" %%i in ('tasklist ^| findstr chrome.exe') do @taskkill /pid %%i /f
-timeout 5
+echo		done.
 
-del "%d%\Bookmarks.bak"
+timeout 5
+echo		done.
+echo.
+
+echo Deleting backup file...
+set b="%d%\Bookmarks.bak"
+if exist %b% del %b%
+echo		done.
+echo.
+
+echo Setting new bookmarks...
 copy ..\..\bookmarks.json "%d%\Bookmarks" /y
+echo		done.
+echo.
+
+echo All Done.
+echo.
 
 pause
