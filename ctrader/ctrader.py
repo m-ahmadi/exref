acd0c4ed855fc643595f5b2d02ee9e27272ed436
m-ahmadi <ahmdee18@gmail.com>
2025-01-06 00:21:37 +0330

    2057

--- ctrader/ctrader.py
+++ ctrader/ctrader.py
@@ -311,7 +311,7 @@ def main():
 	req.ctidTraderAccountId = credentials['accountId']
 	req.period = OAModel.ProtoOATrendbarPeriod.D1
 	req.fromTimestamp = int(dt.datetime(2024,12,1, tzinfo=dt.UTC).timestamp()) * 1000
-	req.toTimestamp = int(dt.datetime(2024,12,31, tzinfo=dt.UTC).timestamp()) * 1000
+	req.toTimestamp = int(dt.datetime(2024,12,31, tzinfo=dt.UTC).timestamp()) * 1000 # or `req.count = <uint32>`
 	deferred = client.send(req)
 	deferred.addCallbacks(onTrendbar, onError)
 
