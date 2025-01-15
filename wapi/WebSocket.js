fa46552d172d63cb753dc53ec925e4246959c5ed
m-ahmadi <ahmdee18@gmail.com>
2025-01-16 03:11:36 +0330

    2079

--- wapi/WebSocket.js
+++ wapi/WebSocket.js
@@ -1,27 +1,31 @@
-var ws = new WebSocket('ws://echo.websocket.org/echo');//ws:http://localhost:1337/web_socket.php');
+// https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API
+
+var endpoint = 'echo.websocket.org/echo';
+//var ws = new WebSocket('ws://'+endpoint);  // without ssl
+var ws = new WebSocket('wss://'+endpoint);   // with ssl
 console.log(ws);
 
-ws.onopen = function(e) {
+ws.onopen = function (e) {
 	console.log('Connection open...', e);
 	
 	ws.send('Hello WebSocket!');
 };
 
-ws.onmessage = function(e) {
-	if(typeof e.data === 'string'){
+ws.onmessage = function (e) {
+	if (typeof e.data === 'string') {
 		console.log('String message received', e, e.data);
 	} else {
 		console.log('Other message received', e, e.data);
 	}
 };
 
-ws.onerror = function(e) {
-	console.log('WebSocket Error: ' , e);
+ws.onerror = function (e) {
+	console.log('WebSocket Error:' , e);
 };
 
 
-ws.onclose = function(e) {
-	console.log('Connection closed', e);
+ws.onclose = function (e) {
+	console.log('Connection closed', e.code, e.reason, e.wasClean);
 };
 
 
