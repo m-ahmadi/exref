fcf410c9c9d06ebb1af2e2740864bd291db79ce8
m-ahmadi <ahmdee18@gmail.com>
2025-01-18 02:11:36 +0330

    2090

--- ctrader/json/get msg ref json files.py
+++ ctrader/json/get msg ref json files.py
@@ -6,3 +6,13 @@ target_keys = filter(lambda i: i.startswith('PROTO_OA_'), dir(OAModel))
 out = dict(map(lambda k: [k, getattr(OAModel, k)], target_keys))
 with open('payloadTypes.json', 'w', encoding='utf-8') as f:
 	json.dump(out, f, ensure_ascii=False, indent=2)
+
+# get OAModel stuff
+target_keys = filter(lambda i: i.startswith('ProtoOA'), dir(OAModel))
+ones_with_keyval = filter(lambda k: hasattr(getattr(OAModel,k),'keys'), target_keys)
+out = {}
+for key in ones_with_keyval:
+  prop = getattr(OAModel, key)
+  out[key] = dict(zip(prop.keys(), prop.values()))
+with open('OAModel.json', 'w', encoding='utf-8') as f:
+	json.dump(out, f, ensure_ascii=False, indent=2)
