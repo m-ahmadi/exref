a272788a41693884952193d6b92d38db948ad7fa
m-ahmadi <ahmdee18@gmail.com>
2025-03-30 03:42:18 +0330

    2137

--- js/arr - split array into chunks.js
+++ js/arr - split array into chunks.js
@@ -23,7 +23,25 @@ function splitArr(arr=[], size=0) {/*mutating*/
 
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // by division size
-// `size` specifies total chunks that array is splited into
+// `size|splits` specifies total chunks that array is splitted into
+
+function arraySplit(arr=[], splits=3) {
+	const len = arr.length;
+	const n = Math.floor(len / splits);
+	const isOdd = len % splits;
+	const res = [];
+	for (let i=0; i<splits; i++) {
+		const start = n * i;
+		let end = start + n;
+		if (i === splits - 1 && isOdd) end = len;
+		const chunk = arr.slice(start, end);
+		res.push(chunk);
+	}
+	return res;
+}
+arraySplit([...Array(9).keys()], 3)  // [ [0,1,2], [3,4,5], [6,7,8] ]
+arraySplit([...Array(10).keys()], 3) // [ [0,1,2], [3,4,5], [6,7,8,9] ]
+
 
 function arraySplit(arr=[], size=0) {/*dynamic chunk size, exactly like `np.array_split()`*/
 	let a = [...arr];
@@ -36,12 +54,16 @@ function arraySplit(arr=[], size=0) {/*dynamic chunk size, exactly like `np.arra
 	}
 	return r;
 }
+arraySplit([...Array(9).keys()], 3)  // [ [0,1,2], [3,4,5], [6,7,8] ]
+arraySplit([...Array(10).keys()], 3) // [ [0,1,2,3], [4,5,6], [7,8,9] ]
+
 
 function arraySplit(arr=[], size=0) {/*static chunk size*/
-  let k = Math.ceil(arr.length / size);
-  let r = [];
+	let k = Math.ceil(arr.length / size);
+	let r = [];
 	for (let c=0,i=0,j=k; c<size; c++,i+=k,j+=k) r.push(arr.slice(i,j));
 	//range(size).reduce(([i,j]) => (r.push(arr.slice(i,j)), [i+k,j+k]), [0,k]); // another way
-  return r;
+	return r;
 }
+
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
