17d657bce9daabfc36138399f1e665c12c8bd5dc
m-ahmadi <ahmdee18@gmail.com>
2025-05-23 11:49:51 +0330

    2168

--- misc/scrap - domain prices.js
+++ misc/scrap - domain prices.js
@@ -1,11 +1,20 @@
 // https://www.iranserver.com/domains/tld
 
-a = [...document.querySelectorAll('#dpanels_pro .col-md-6 .col-4.ltr')].map(i=>i.innerText)
-b = [...document.querySelectorAll('#dpanels_pro .col-md-6 .col-4.secondary')].map(i=>i.innerText).filter((v,i)=>i%2===0)
-c = [...document.querySelectorAll('#dpanels_pro .col-md-6 .col-4.secondary')].map(i=>i.innerText).filter((v,i)=>i%2!==0)
-csv = a.map((v,i) => [v, '"'+b[i]+'"', '"'+c[i]+'"'] ).map(i=>i.join(',')).join('\n')
+// domains with discount
+items = [...document.querySelectorAll('#dpanels_pro .row.tld-item')].map(el => ({
+	domain: el.children.item(0).innerText,
+	price: +el.children.item(1).innerText.replaceAll(',',''),
+	renew: +el.children.item(2).innerText.replaceAll(',',''),
+}));
+items.sort(({renew:a},{renew:b}) => a-b);
+items.sort(({price:a},{price:b}) => a-b);
+console.table(items);
 
 
-a = [...document.querySelectorAll('#dpanels .col-6.ltr')].map(i=>i.innerText)
-b = [...document.querySelectorAll('#dpanels .col-6.secondary')].map(i=>i.innerText)
-csv = a.map((v,i) => [v, '"'+b[i]+'"'] ).map(i=>i.join(',')).join('\n')
+// normal domains
+items = [...document.querySelectorAll('#dpanels .row.tld-item')].map(el => ({
+	domain: el.children.item(0).innerText,
+	price: +el.children.item(1).innerText.replaceAll(',',''),
+}));
+items.sort(({price:a},{price:b}) => a-b);
+console.table(items);
