ce0bdd8019347bcfbd4e081605401b63446266b1
m-ahmadi <ahmdee18@gmail.com>
2025-08-05 04:39:00 +0330

    2198

---
+++ ppkg/websockets.py
@@ -0,0 +1,68 @@
+# https://websockets.readthedocs.io/en/stable
+import websockets # pip install websockets
+# websockets --version
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# examples
+
+# client - async
+from websockets.asyncio.client import connect
+async def main():
+	async with connect('wss://echo.websocket.org') as ws:
+		await ws.send('hello')
+		server_msg = await ws.recv()
+		print('server sent:', server_msg)
+aio.run(main())
+
+
+# client - async - never-breaking process (infinte loop)
+from websockets.asyncio.client import connect
+async def main():
+	async with connect('wss://echo.websocket.org') as ws:
+		while True:
+			try:
+				await ws.send('hi')
+				await aio.sleep(2)
+				server_msg = await ws.recv()
+				print('server sent:', server_msg)
+			except websockets.exceptions.ConnectionClosed:
+					break
+aio.run(main())
+
+
+# client - sync
+from websockets.sync.client import connect
+def main():
+	with connect('wss://echo.websocket.org') as ws:
+		ws.send('hello')
+		server_msg = ws.recv()
+		print('server sent:', server_msg)
+main()
+
+
+# client - reconnect automatically on errors
+from websockets.asyncio.client import connect
+async def main():
+	async for ws in connect('wss://echo.websocket.org'):
+		# infinite async iterator
+		try:
+			await ws.send('hello')
+			server_msg = await ws.recv()
+			print('server sent:', server_msg)
+		except websockets.exceptions.ConnectionClosed:
+			continue
+aio.run(main())
+
+
+# server - listen forever, print msg sent by client, echo back to client
+import asyncio as aio
+from websockets.asyncio.server import serve
+async def handler(ws):
+	while True:
+		message = await ws.recv()
+		print('client sent:\t', message)
+		await ws.send(message) # echoing back
+async def main():
+	async with serve(handler, '127.0.0.1', 8001) as server:
+		await server.serve_forever()
+aio.run(main())
---
+++ py/m - asyncio.py
@@ -0,0 +1,49 @@
+import asyncio as aio
+# https://docs.python.org/3/library/asyncio.html
+
+aio.create_task()
+aio.sleep()
+aio.run()
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# examples
+import time
+# awaitables: coroutine, Task, Future
+
+# coroutine
+async def main():
+	print('hello')
+	await aio.sleep(1)
+	await aio.sleep(10, result='hi')
+	print('world')
+aio.run(main())
+
+
+
+# sequential
+async def say_after(delay, what):
+	await aio.sleep(delay)
+	print(what)
+async def main():
+	await say_after(1, 'hello')
+	await say_after(2, 'world')
+aio.run(main())
+
+
+
+# concurrent
+async def main():
+	task1 = aio.create_task(say_after(1, 'hello'))
+	task2 = aio.create_task(say_after(2, 'world'))
+	print(f"started at {time.strftime('%X')}")
+	await task1
+	await task2
+	print(f"finished at {time.strftime('%X')}")
+aio.run(main())
+# or
+async def main():
+	async with aio.TaskGroup() as tg:
+		# the await is implicit when the context manager exits
+		task1 = tg.create_task(say_after(1, 'hello'))
+		task2 = tg.create_task(say_after(2, 'world'))
+aio.run(main())
