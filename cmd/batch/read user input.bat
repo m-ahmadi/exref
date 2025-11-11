cd209ba4733a23b606133abf6e497c5908a71940
m-ahmadi <ahmdee18@gmail.com>
2025-11-11 07:52:20 +0330

    2290

--- cmd/batch/read user input.bat
+++ cmd/batch/read user input.bat
@@ -6,4 +6,7 @@ echo Hello %name%!
 set /p lastname="What is you last name? "
 echo Interesting last name Mr. %lastname%?
 
+set /p job="What is your job? " || set job=unemployed
+echo Oh!, so you are %job%
+
 pause
