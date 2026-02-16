d3a5ee917e0801735089c7586f02b02f0090c68f
m-ahmadi <ahmdee18@gmail.com>
2026-02-16 17:44:39 +0330

    2338

---
+++ node/sol - detect locale.js
@@ -0,0 +1,10 @@
+Intl.DateTimeFormat().resolvedOptions() /*
+{
+	locale: 'en-US',
+	calendar: 'gregory',
+	numberingSystem: 'latn',
+	timeZone: 'Asia/Tehran',
+	year: 'numeric',
+	month: 'numeric',
+	day: 'numeric'
+} */
